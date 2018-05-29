9163b60775e33cd6fc750cc324bb40bc3253d5a1
Robert Lee-Cann <leeky@leeky.org.uk>
2018-05-29 15:42:17 +0100

    (chore) Fetching a framework also returns its lots

--- app/views/v1/frameworks/show.json.jbuilder
+++ app/views/v1/frameworks/show.json.jbuilder
@@ -1,2 +1,4 @@
 json.name @framework.name
 json.short_name @framework.short_name
+
+json.lots @framework.lots
--- spec/requests/v1/frameworks_spec.rb
+++ spec/requests/v1/frameworks_spec.rb
@@ -37,5 +37,27 @@ RSpec.describe '/v1' do
 
       expect(response.body).to include_json(expected)
     end
+
+    it 'includes the list of lots on that framework' do
+      framework = FactoryBot.create(:framework, name: 'Cheese Board 8', short_name: 'cboard8')
+
+      FactoryBot.create(:framework_lot, number: '1a', framework: framework)
+      FactoryBot.create(:framework_lot, number: '1b', framework: framework)
+
+      get "/v1/frameworks/#{framework.id}"
+
+      expect(response).to be_successful
+
+      expected = {
+        name: 'Cheese Board 8',
+        short_name: 'cboard8',
+        lots: [
+          { number: '1a' },
+          { number: '1b' }
+        ]
+      }
+
+      expect(response.body).to include_json(expected)
+    end
   end
 end
