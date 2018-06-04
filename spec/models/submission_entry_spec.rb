8d321c1e800043debfbe7ff75b17a2e2d5c04018
Robert Lee-Cann <leeky@leeky.org.uk>
2018-06-04 15:50:55 +0100

    (chore) When creating a SubmissionEntry, data is required
    There's no point to create a `SubmissionEntry` without submitting any
    data, so make sure we throw an error when this occurs.

--- spec/models/submission_entry_spec.rb
+++ spec/models/submission_entry_spec.rb
@@ -3,4 +3,6 @@ require 'rails_helper'
 RSpec.describe SubmissionEntry do
   it { is_expected.to belong_to(:submission) }
   it { is_expected.to belong_to(:submission_file) }
+
+  it { is_expected.to validate_presence_of(:data) }
 end
--- spec/requests/v1/files_spec.rb
+++ spec/requests/v1/files_spec.rb
@@ -25,5 +25,21 @@ RSpec.describe '/v1' do
       expect(json['source']['row']).to eql 42
       expect(json['data']['test']).to eql 'test'
     end
+
+    it 'returns an error if the "data" parameter is omitted' do
+      file = FactoryBot.create(:submission_file)
+
+      params = {
+        source: { sheet: 'Orders', row: 1 }
+      }
+
+      headers = {
+        'CONTENT_TYPE': 'application/json'
+      }
+
+      post "/v1/files/#{file.id}/entries", params: params.to_json, headers: headers
+
+      expect(response).to have_http_status(:bad_request)
+    end
   end
 end
