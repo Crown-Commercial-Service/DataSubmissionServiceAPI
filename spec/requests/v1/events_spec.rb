f2fa4e26352e72b27b8a42c2d5b6becf0090da55
Robert Lee-Cann <leeky@leeky.org.uk>
2018-06-13 10:35:16 +0100

    (feature) Endpoint for recording sign-out events in the audit log

--- spec/requests/v1/events_spec.rb
+++ spec/requests/v1/events_spec.rb
@@ -11,4 +11,15 @@ RSpec.describe '/v1' do
       expect(event_store).to have_published(an_event(UserSignedIn).with_data(user_id: '1234'))
     end
   end
+
+  describe 'POST /events/user_signed_out' do
+    it 'stores the UserSignedOut event to the audit log' do
+      event_store = RailsEventStore::Client.new
+
+      post '/v1/events/user_signed_out?user_id=5678'
+
+      expect(response).to have_http_status(:created)
+      expect(event_store).to have_published(an_event(UserSignedOut).with_data(user_id: '5678'))
+    end
+  end
 end
