0703834ae7a9daf03c4de4012923c10170c2409c
Robert Lee-Cann <leeky@leeky.org.uk>
2018-07-05 11:53:34 +0100

    (feature) Memberships can be filtered by supplier_id and/or user_id

--- app/controllers/v1/memberships_controller.rb
+++ app/controllers/v1/memberships_controller.rb
@@ -1,5 +1,9 @@
 class V1::MembershipsController < ApplicationController
   def index
-    render jsonapi: Membership.all
+    memberships = Membership.where(nil)
+    memberships = memberships.where(supplier_id: params.dig(:filter, :supplier_id)) if params.dig(:filter, :supplier_id)
+    memberships = memberships.where(user_id: params.dig(:filter, :user_id)) if params.dig(:filter, :user_id)
+
+    render jsonapi: memberships
   end
 end
--- spec/requests/v1/memberships_spec.rb
+++ spec/requests/v1/memberships_spec.rb
@@ -20,5 +20,41 @@ RSpec.describe '/v1' do
       expect(json['data'][1]).to have_attribute(:user_id).with_value(user_id)
       expect(json['data'][1]).to have_attribute(:supplier_id).with_value(supplier2.id)
     end
+
+    it 'can be filtered by supplier_id' do
+      supplier1 = FactoryBot.create(:supplier)
+      supplier2 = FactoryBot.create(:supplier)
+
+      user_id = 'e3913698-f379-4004-9dd8-98053f629a00'
+
+      Membership.create!(supplier: supplier1, user_id: user_id)
+      Membership.create!(supplier: supplier2, user_id: user_id)
+
+      get "/v1/memberships?filter[supplier_id]=#{supplier1.id}"
+
+      expect(response).to have_http_status(:ok)
+
+      expect(json['data'].size).to eql 1
+      expect(json['data'][0]).to have_attribute(:supplier_id).with_value(supplier1.id)
+      expect(json['data'][0]).to have_attribute(:user_id).with_value(user_id)
+    end
+
+    it 'can be filtered by user_id' do
+      supplier = FactoryBot.create(:supplier)
+
+      included_user_id = 'e3913698-f379-4004-9dd8-98053f629a00'
+      excluded_user_id = '2f0c98f2-5ce3-4ce3-9214-159cb2dbe276'
+
+      Membership.create!(supplier: supplier, user_id: included_user_id)
+      Membership.create!(supplier: supplier, user_id: excluded_user_id)
+
+      get "/v1/memberships?filter[user_id]=#{included_user_id}"
+
+      expect(response).to have_http_status(:ok)
+
+      expect(json['data'].size).to eql 1
+      expect(json['data'][0]).to have_attribute(:supplier_id).with_value(supplier.id)
+      expect(json['data'][0]).to have_attribute(:user_id).with_value(included_user_id)
+    end
   end
 end
