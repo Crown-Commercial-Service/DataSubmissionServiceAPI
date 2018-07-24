1c8bed469455112d9a5a9072f0f8fd3f9da40782
Tekin Suleyman <tekin@tekin.co.uk>
2018-07-24 10:08:52 +0100

    Update seeds.rb for local testing
    This seeds the database with a single supplier that has multiple tasks
    in various states:
    
    - An unstarted task with no submissions
    - A started task with the state "in_progress" (submission created but no
    file/ingest started) started)
    - A started task with the state "processing" (a submission has been made
    and the file is being processed)
    - A started task with a valid submission (all rows have been processed
    and are valud)
    - A started task with an invalid submission (all rows have been
    processed and some have errors)
    - A successfully completed task (the submission has been validated and
    marked as completed)
    
    Note that these seeds represent the idealised state of the world, but
    not necessarily the state that the system currently puts the submissions
    into. for example, there is no part of the system that currently
    transitions a "in_progress" task to "processing" when ingest starts. Our
    next task is to make sure the system performs the transitions as
    required so that the task and submission always have an up-to-date
    state.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,102 @@
-framework = Framework.create!(short_name: 'cboard8', name: 'Cheese Board 8')
-framework.lots.create!(number: '1', description: 'Soft cheese')
-framework.lots.create!(number: '2', description: 'Hard cheese')
+supplier = Supplier.find_or_create_by!(name: 'Bobs Cheese Shop')
+due_date = Date.today.next_month.change(day: 7)
+
+
+framework_unstarted = Framework.find_or_create_by!(short_name: 'cboard8', name: 'Cheese Board 8')
+Agreement.find_or_create_by!(supplier: supplier, framework: framework_unstarted)
+Task.find_or_create_by!(
+  status: :unstarted,
+  due_on: due_date,
+  framework: framework_unstarted,
+  supplier: supplier,
+  period_month: Date.today.month,
+  period_year: Date.today.year,
+  description: 'Unstarted task'
+)
+
+
+framework_pending = Framework.find_or_create_by!(short_name: 'cboard9', name: 'Cheese Board 9')
+Agreement.find_or_create_by!(supplier: supplier, framework: framework_pending)
+in_progress_task = Task.find_or_create_by!(
+  status: :in_progress,
+  due_on: due_date,
+  framework: framework_pending,
+  supplier: supplier,
+  period_month: Date.today.month,
+  period_year: Date.today.year,
+  description: 'In progress task (pending submission)'
+)
+supplier.submissions.find_or_create_by!(framework: framework_pending, task: in_progress_task)
+
+
+framework_processing = Framework.find_or_create_by!(short_name: 'cboard10', name: 'Cheese Board 10')
+Agreement.find_or_create_by!(supplier: supplier, framework: framework_processing)
+task_processing = Task.find_or_create_by!(
+  status: :in_progress,
+  due_on: due_date,
+  framework: framework_processing,
+  supplier: supplier,
+  period_month: Date.today.month,
+  period_year: Date.today.year,
+  description: 'In progress task (processing submission)'
+)
+processing_submission = supplier.submissions.find_or_create_by!(framework: framework_processing, task: task_processing, aasm_state: "processing")
+submission_file = processing_submission.files.find_or_create_by!(rows: 2)
+processing_submission.entries.find_or_create_by!(submission_file: submission_file, data: { key: "value" }, source: { sheet: "InvoicesReceived", row: 1 })
+processing_submission.entries.find_or_create_by!(submission_file: submission_file, data: { key: "another value" }, source: { sheet: "InvoicesReceived", row: 2 })
+
+
+framework_valid = Framework.find_or_create_by!(short_name: 'cboard11', name: 'Cheese Board 11')
+Agreement.find_or_create_by!(supplier: supplier, framework: framework_valid)
+task_in_review = Task.find_or_create_by!(
+  status: :in_progress,
+  due_on: due_date,
+  framework: framework_valid,
+  supplier: supplier,
+  period_month: Date.today.month,
+  period_year: Date.today.year,
+  description: 'In review task (validated submission)'
+)
+valid_submission = supplier.submissions.find_or_create_by!(framework: framework_valid, task: task_in_review, aasm_state: "in_review")
+submission_file = valid_submission.files.find_or_create_by!(rows: 2)
+valid_submission.entries.find_or_create_by!(submission_file: submission_file, aasm_state: "validated", data: { key: "value" }, source: { sheet: "InvoicesReceived", row: 1 })
+valid_submission.entries.find_or_create_by!(submission_file: submission_file, aasm_state: "validated", data: { key: "another value" }, source: { sheet: "InvoicesReceived", row: 2 })
+
+
+framework_invalid = Framework.find_or_create_by!(short_name: 'cboard12', name: 'Cheese Board 12')
+Agreement.find_or_create_by!(supplier: supplier, framework: framework_invalid)
+task_in_review_with_errors = Task.find_or_create_by!(
+  status: :in_progress,
+  due_on: due_date,
+  framework: framework_invalid,
+  supplier: supplier,
+  period_month: Date.today.month,
+  period_year: Date.today.year,
+  description: 'In review task (invalid submission)'
+)
+invalid_submission = supplier.submissions.find_or_create_by!(framework: framework_invalid, task: task_in_review_with_errors, aasm_state: "in_review")
+submission_file = invalid_submission.files.find_or_create_by!(rows: 2)
+invalid_submission.entries.find_or_create_by!(submission_file: submission_file, aasm_state: "validated", data: { key: "value" }, source: { sheet: "InvoicesReceived", row: 1 })
+invalid_submission.entries.find_or_create_by!(
+  submission_file: submission_file,
+  aasm_state: "errored",
+  data: { key: "" },
+  source: { sheet: "InvoicesReceived", row: 2 },
+).update_attribute(:validation_errors, [{ "message": "Required value error", "location": { "row": 2, "column": 1 } }])
+
+# completed task
+framework_completed = Framework.find_or_create_by!(short_name: 'cboard13', name: 'Cheese Board 13')
+Agreement.find_or_create_by!(supplier: supplier, framework: framework_completed)
+task_completed = Task.find_or_create_by!(
+  status: :completed,
+  due_on: due_date,
+  framework: framework_completed,
+  supplier: supplier,
+  period_month: Date.today.month,
+  period_year: Date.today.year,
+  description: 'Completed task'
+)
+valid_submission = supplier.submissions.find_or_create_by!(framework: framework_completed, task: task_completed, aasm_state: "completed")
+submission_file = valid_submission.files.find_or_create_by!(rows: 2)
+valid_submission.entries.find_or_create_by!(submission_file: submission_file, aasm_state: "validated", data: { key: "value" }, source: { sheet: "InvoicesReceived", row: 1 })
+valid_submission.entries.find_or_create_by!(submission_file: submission_file, aasm_state: "validated", data: { key: "another value" }, source: { sheet: "InvoicesReceived", row: 2 })
