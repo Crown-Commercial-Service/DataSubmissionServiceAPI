14a012ce687638247380a383506fefb69a9a9cbd
Tekin Suleyman <tekin@tekin.co.uk>
2018-08-09 10:36:39 +0100

    Add CodaFinanceReport for reporting submissions
    This will be used to generate CSV reports for completed submission to be
    fed into the current finance system, Coda. For now we're generating this
    report manually, thus it outputs as a string to make it easy to run the
    report and copy the output from the console.
    
    Eventually this report will need to be generated on a schedule daily,
    with the resulting file put somewhere for CCS to perform some automatic
    processing and importing.

---
+++ app/models/reports/coda_finance_report.rb
@@ -0,0 +1,77 @@
+require 'csv'
+
+module Reports
+  # Used to generate reports summarising submissions in the format needed for
+  # feeding into Coda, the CCS finance system.
+  class CodaFinanceReport
+    attr_reader :submissions
+
+    HEADERS = [
+      'RunID',
+      'Nominal',
+      'Customer Code',
+      'Customer Name',
+      'Contract ID',
+      'Order Number',
+      'Lot Description',
+      'Inf Sales',
+      'Commission',
+      'Commission %',
+      'End User',
+      'Submitter',
+      'Month',
+      'M_Q'
+    ].freeze
+
+    def initialize(submissions)
+      @submissions = submissions
+    end
+
+    def to_csv
+      CSV.generate write_headers: true, headers: HEADERS do |csv|
+        submissions.each do |submission|
+          csv << row_for_submission(submission, Customer.sectors[:central_government])
+          csv << row_for_submission(submission, Customer.sectors[:wider_public_sector])
+        end
+      end
+    end
+
+    private
+
+    def row_for_submission(submission, sector)
+      data = CodaFinanceReportRow.new(submission, sector).data
+
+      [
+        report_uid,
+        data['Nominal'],
+        data['Customer Code'],
+        data['Customer Name'],
+        data['Contract ID'],
+        data['Order Number'],
+        data['Lot Description'],
+        data['Inf Sales'],
+        data['Commission'],
+        data['Commission %'],
+        data['End User'],
+        data['Submitter'],
+        data['Month'],
+        monthly_or_quarterly
+      ]
+    end
+
+    # CCS require that each report has a unique reference in case they need to
+    # perform an audit and trace a particular entry in the finance system back
+    # to the report that generated it.
+    def report_uid
+      "DSS-#{Time.zone.now.to_i}"
+    end
+
+    # Used to indicate if the data in the report is for monthly or quarterly
+    # submissions. In reality, only monthly submissions are supported by both
+    # the legacy and the new system and quarterly frameworks (of which there is
+    # one?) is handled manually by CCS.
+    def monthly_or_quarterly
+      'M'
+    end
+  end
+end
---
+++ spec/models/reports/coda_finance_report_spec.rb
@@ -0,0 +1,63 @@
+require 'rails_helper'
+
+RSpec.describe Reports::CodaFinanceReport do
+  let(:home_office) { FactoryBot.create(:customer, :central_government, name: 'Home Office') }
+  let(:framework) { FactoryBot.create(:framework, coda_reference: 409999, name: 'G CLOUD', short_name: 'RM3787') }
+  let(:task_1) do
+    FactoryBot.create(:task, framework: framework, supplier: supplier_1, period_month: 8, period_year: 2018)
+  end
+  let(:task_2) do
+    FactoryBot.create(:task, framework: framework, supplier: supplier_2, period_month: 8, period_year: 2018)
+  end
+  let(:supplier_1) { FactoryBot.create(:supplier, name: 'Bob', coda_reference: 'C099999') }
+  let(:supplier_2) { FactoryBot.create(:supplier, name: 'Mary', coda_reference: 'C011111') }
+
+  let(:no_business_submission) do
+    FactoryBot.create(:no_business_submission, framework: framework, supplier: supplier_1, task: task_1)
+  end
+  let(:submission) do
+    FactoryBot.create(
+      :submission,
+      entries: [submission_entry_1, submission_entry_2],
+      supplier: supplier_2,
+      framework: framework,
+      task: task_1
+    )
+  end
+  let(:submission_entry_1) do
+    FactoryBot.build(
+      :validated_submission_entry,
+      data: { 'Total Cost (ex VAT)' => '678.55', 'Customer URN' => home_office.urn },
+      source: { sheet: 'InvoicesRaised', row: 1 }
+    )
+  end
+  let(:submission_entry_2) do
+    FactoryBot.build(
+      :validated_submission_entry,
+      data: { 'Total Cost (ex VAT)' => '123.45', 'Customer URN' => home_office.urn },
+      source: { sheet: 'InvoicesRaised', row: 2 }
+    )
+  end
+
+  let(:submissions) { [submission, no_business_submission] }
+  let(:report) { Reports::CodaFinanceReport.new(submissions) }
+  let(:report_timestamp) { Time.zone.now.to_i }
+
+  let(:expected_csv) do
+    <<~CSV
+      RunID,Nominal,Customer Code,Customer Name,Contract ID,Order Number,Lot Description,Inf Sales,Commission,Commission %,End User,Submitter,Month,M_Q
+      DSS-#{report_timestamp},409999,C011111,Mary,RM3787,,G CLOUD,802.00,12.03,0.015,UCGV,Mary,August 2018,M
+      DSS-#{report_timestamp},409999,C011111,Mary,RM3787,,G CLOUD,0.00,0.00,0.015,UWPS,Mary,August 2018,M
+      DSS-#{report_timestamp},409999,C099999,Bob,RM3787,,G CLOUD,0.00,0.00,0.015,UCGV,Bob,August 2018,M
+      DSS-#{report_timestamp},409999,C099999,Bob,RM3787,,G CLOUD,0.00,0.00,0.015,UWPS,Bob,August 2018,M
+    CSV
+  end
+
+  around(:example) do |example|
+    freeze_time { example.run }
+  end
+
+  it 'generates a CSV file based on the submissions provided' do
+    expect(report.to_csv).to eq expected_csv
+  end
+end
