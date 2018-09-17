3a73061ace010b9894974db7a9ac5ee1e5dd728a
Russell Garner <rgarner@zephyros-systems.co.uk>
2018-09-17 15:01:29 +0100

    Add invoice and order count to extract query
    Use ActiveRecord projection to get the counts of both invoices and
    orders (also known as contracts) in a single query.  Put these in
    underscore methods _invoice_entry_count and _order_entry_count for
    the exclusive use of Export::Submissions::Row.

--- app/models/export/submissions/extract.rb
+++ app/models/export/submissions/extract.rb
@@ -4,7 +4,15 @@ module Export
       RELEVANT_STATUSES = %w[completed validation_failed].freeze
 
       def self.all_relevant
-        Submission.where(aasm_state: RELEVANT_STATUSES)
+        Submission.select('submissions.*,' \
+                          'COUNT(DISTINCT orders) AS _order_entry_count,' \
+                          'COUNT(DISTINCT invoices) AS _invoice_entry_count')
+                  .joins('LEFT JOIN submission_entries orders ON ' \
+                         "(orders.submission_id = submissions.id AND orders.entry_type = 'order')")
+                  .joins('LEFT JOIN submission_entries invoices ON '\
+                         "(invoices.submission_id = submissions.id AND invoices.entry_type = 'invoice')")
+                  .where(aasm_state: RELEVANT_STATUSES)
+                  .group('submissions.id') # Postgres lazy group can use ID only
       end
     end
   end
--- spec/factories/submission.rb
+++ spec/factories/submission.rb
@@ -10,8 +10,8 @@ FactoryBot.define do
     end
 
     transient do
-      invoice_entries { 0 }
-      order_entries { 0 }
+      invoice_entries 0
+      order_entries 0
     end
 
     after(:create) do |submission, evaluator|
@@ -20,10 +20,8 @@ FactoryBot.define do
     end
 
     factory :submission_with_pending_entries do
-      after(:create) do |submission, _evaluator|
-        create_list(:invoice_entry, 2, submission: submission)
-        create_list(:order_entry, 1, submission: submission)
-      end
+      invoice_entries 2
+      order_entries 1
     end
 
     factory :submission_with_validated_entries do
