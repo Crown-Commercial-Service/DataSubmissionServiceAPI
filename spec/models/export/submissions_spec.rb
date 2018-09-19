37f145878c35548851f2193976f6999bbc5e9ad7
Russell Garner <rgarner@zephyros-systems.co.uk>
2018-09-19 17:02:36 +0100

    Make export_submissions_spec a better integration test
    What we *should* be doing:
    
    - Going outside-in from the rake task and having the rake task
      effectively be the end-to-end test
    
    What we *are* doing:
    
    - Using the rake task as little more than a UI test (not good)
    
    Without addressing that last problem for the moment, make
    export/submissions_spec do the end-to-end testing by properly
    creating the object graph and intentionally coupling (for now) the spec
    for Export::Submissions with the results of
    `Export::Submissions::Extract#all_relevant`.

--- spec/models/export/submissions_spec.rb
+++ spec/models/export/submissions_spec.rb
@@ -2,35 +2,35 @@ require 'rails_helper'
 require 'stringio'
 
 RSpec.describe Export::Submissions do
-  context 'given valid submissions and an in-memory output' do
-    let(:task) { create(:task) }
+  context 'given some submissions and an in-memory output' do
+    let!(:no_business_submission) do
+      create(:no_business_submission)
+    end
 
-    let(:submission) do
+    let!(:submission) do
       create(
         :submission,
-        task: task,
         aasm_state: 'completed',
         created_at: Time.zone.local(2018, 9, 18, 14, 20, 35),
         management_charge: 45000,
         purchase_order_number: 'PO1234',
+        files: [
+          create(:submission_file, :with_attachment)
+        ],
         entries: [
-          create(:submission_entry),
-          create(:submission_entry)
+          create(:invoice_entry),
+          create(:order_entry)
         ]
       )
     end
 
-    let(:submissions) { [submission, create(:submission, aasm_state: 'validation_failed')] }
+    let(:extracted_submissions) { Export::Submissions::Extract.all_relevant }
 
-    let(:output) { StringIO.new }
+    let(:output)           { StringIO.new }
     subject(:output_lines) { output.string.split("\n") }
 
     before do
-      # Stub projected fields - fields that have been created by Export::Submissions::Extract
-      allow_any_instance_of(Export::Submissions::Row).to receive(:invoice_entry_count).and_return(1)
-      allow_any_instance_of(Export::Submissions::Row).to receive(:order_entry_count).and_return(1)
-      allow_any_instance_of(Export::Submissions::Row).to receive(:submission_file_type).and_return('xls')
-      Export::Submissions.new(submissions, output).run
+      Export::Submissions.new(extracted_submissions, output).run
     end
 
     it 'writes a header to that output' do
@@ -43,7 +43,7 @@ RSpec.describe Export::Submissions do
 
     it 'writes each submission to that output' do
       expect(output_lines.length).to eql(3)
-      expect(output_lines[1]).to eql(
+      expect(output_lines.find { |line| line.match('PO1234') }).to eql(
         "#{submission.task.id},#{submission.id},supplier_accepted,file,xls,1,#MISSING,1,#MISSING,450.00," \
         '0.015,2018-09-18T14:20:35Z,,,,,PO1234'
       )
@@ -51,7 +51,7 @@ RSpec.describe Export::Submissions do
 
     it 'has as many headers as row values' do
       expect(Export::Submissions::HEADER.length).to eql(
-        Export::Submissions::Row.new(submission).row_values.length
+        Export::Submissions::Row.new(extracted_submissions.first).row_values.length
       )
     end
   end
