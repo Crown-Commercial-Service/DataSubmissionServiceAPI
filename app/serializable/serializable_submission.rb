bdd0a193e59e192f2710d90a494b7add09c82db5
Tekin Suleyman <tekin@tekin.co.uk>
2018-09-24 10:01:58 +0100

    Remove submission entries association from API
    We no longer need a submissions entries to be exposed in the API so
    remving it as an option to prevent misuse.

--- app/serializable/serializable_submission.rb
+++ app/serializable/serializable_submission.rb
@@ -5,7 +5,6 @@ class SerializableSubmission < JSONAPI::Serializable::Resource
 
   belongs_to :framework
   belongs_to :task
-  has_many :entries
   has_many :files
 
   attributes :framework_id, :supplier_id, :task_id
