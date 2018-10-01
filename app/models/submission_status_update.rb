9c5ad25e6cd6242229096650c426a19f37bd0f7d
Tekin Suleyman <tekin@tekin.co.uk>
2018-10-01 13:31:20 +0100

    Temporarily bypass management charge calculation
    We've realised that we [removed an endpoint the calculator relies] on to
    perform the calculation, which has broken calculation! For now we're
    going to skip calculation so that users are not blocked from completing
    their submissions.
    
    We will need to re-instate calculation and perform calculation against
    October submissions before we can generate the finance and date
    warehouse exports.
    
    [removed the endpoint that the calculator relies](https://github.com/dxw/DataSubmissionServiceAPI/commit/bdd0a193e59e192f2710d90a494b7add09c82db5)

--- app/models/submission_status_update.rb
+++ app/models/submission_status_update.rb
@@ -8,29 +8,12 @@ class SubmissionStatusUpdate
   def perform!
     return unless submission.entries.any?
 
-    trigger_management_charge_calculation if all_entries_valid?
-    transition_to_in_review if no_entries_pending? && some_entries_errored?
+    submission.ready_for_review! if no_entries_pending?
   end
 
   private
 
-  def all_entries_valid?
-    submission.entries.validated.count == submission.entries.count
-  end
-
-  def transition_to_in_review
-    submission.ready_for_review!
-  end
-
   def no_entries_pending?
     submission.entries.pending.none?
-  end
-
-  def some_entries_errored?
-    submission.entries.errored.any?
-  end
-
-  def trigger_management_charge_calculation
-    AWSLambdaService.new(submission_id: submission.id).trigger
   end
 end
