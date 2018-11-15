31c80f0e6b86b259b022a4369c90a943358667ce
James Darling <james@abscond.org>
2018-11-15 16:28:44 +0000

    Add specs for http auth

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,4 +1,11 @@
 class APIController < ActionController::API
   include ActionController::HttpAuthentication::Basic::ControllerMethods
-  http_basic_authenticate_with name: "dxw", password: ENV['API_PASSWORD'] if ENV['API_PASSWORD']
+  before_action :authenticate
+
+  def authenticate
+    return if ENV['API_PASSWORD'].blank?
+    authenticate_or_request_with_http_basic('Administration') do |username, password|
+      username == 'dxw' && password == ENV['API_PASSWORD']
+    end
+  end
 end
