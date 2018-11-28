f5ba6c8aa668c9830dfdeaaea27dfa3ce299b479
James Darling <james@abscond.org>
2018-11-28 14:13:08 +0000

    Admin can link and unlink user from suppliers

---
+++ app/controllers/admin/memberships_controller.rb
@@ -0,0 +1,31 @@
+class Admin::MembershipsController < AdminController
+  before_action :find_user
+  def new
+    @suppliers = Supplier.all - @user.suppliers
+  end
+
+  def create
+    @user.memberships.create!(membership_params)
+    redirect_to admin_user_path(@user)
+  end
+
+  def show
+    @membership = @user.memberships.find(params[:id])
+  end
+
+  def destroy
+    @membership = @user.memberships.find(params[:id])
+    @membership.destroy
+    redirect_to admin_user_path(@user)
+  end
+
+  private
+
+  def find_user
+    @user = User.find(params[:user_id])
+  end
+
+  def membership_params
+    params.require(:membership).permit(:supplier_id)
+  end
+end
