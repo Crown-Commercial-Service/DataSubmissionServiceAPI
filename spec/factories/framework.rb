959a6d928e3f91257dd6ff2b0ba80d32792ea5c6
Tekin Suleyman <tekin@tekin.co.uk>
2018-12-04 16:54:10 +0000

    Tweak Framework factory to avoid short_name clash
    This is resulting in sporadic test failures.
    
    Because there are some specs that test against the actual framework
    definitions and thus need to create framework records with matching
    short names such as "RM1070", we can end up in a situation where the
    factory attempts to creates a framework with a clashing short name
    (e.g. when the sequence number has reached 70 in a spec that also
    creates a framework with the explicit short_name of "RM1070"). By
    changing the format to one that can't match any existing frameworks (at
    least any that we know of) we avoid these flakey tests.

--- spec/factories/framework.rb
+++ spec/factories/framework.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :framework do
-    sequence(:short_name) { |n| "RM#{n + 1000}" }
+    sequence(:short_name) { |n| "FM#{n + 1000}" }
     sequence(:name) { |n| "G Cloud #{n}" }
   end
 end
