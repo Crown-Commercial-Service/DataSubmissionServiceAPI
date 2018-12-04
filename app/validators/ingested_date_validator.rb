36a7e8ec8898bc0a22a10df6f1519a13fc31f65e
Tekin Suleyman <tekin@tekin.co.uk>
2018-12-04 11:20:12 +0000

    Better error message on optional date fields
    We can introspect to see if the date field is optional and give the user
    a more appropriate error message so that they know to leave the field
    blank.

--- app/validators/ingested_date_validator.rb
+++ app/validators/ingested_date_validator.rb
@@ -4,6 +4,17 @@ class IngestedDateValidator < ActiveModel::EachValidator
   include StringUtils
 
   def validate_each(record, attribute, value)
-    record.errors.add(attribute, :invalid_ingested_date) unless parse_date_string(value)
+    record.errors.add(attribute, error_message(record, attribute)) unless parse_date_string(value)
+  end
+
+  private
+
+  def error_message(record, attribute)
+    attribute_optional?(record, attribute) ? :invalid_optional_ingested_date : :invalid_ingested_date
+  end
+
+  def attribute_optional?(record, attribute)
+    validator = record.class.validators_on(attribute).find { |v| v.is_a?(IngestedDateValidator) }
+    validator.options[:allow_nil]
   end
 end
--- spec/validators/ingested_date_validator_spec.rb
+++ spec/validators/ingested_date_validator_spec.rb
@@ -1,43 +1,75 @@
 require 'rails_helper'
 
 RSpec.describe IngestedDateValidator do
-  let(:sheet_class) do
-    Class.new(Framework::Sheet) do
-      extend ActiveModel::Naming
+  context 'validating a field' do
+    let(:sheet_class) do
+      Class.new(Framework::Sheet) do
+        extend ActiveModel::Naming
 
-      def self.name
-        'Validator'
+        def self.name
+          'Validator'
+        end
+
+        field 'An Ingested Date', :string, ingested_date: true
       end
-
-      field 'An Ingested Date', :string, ingested_date: true
     end
-  end
 
-  it 'validates DD/MM/YYYY date strings' do
-    ['12/10/2018', '1/1/2018', '21/9/2017'].each do |valid_date_string|
-      instance = sheet_class.new_from_params('An Ingested Date' => valid_date_string)
-      expect(instance).to be_valid
+    it 'validates DD/MM/YYYY date strings' do
+      ['12/10/2018', '1/1/2018', '21/9/2017'].each do |valid_date_string|
+        instance = sheet_class.new_from_params('An Ingested Date' => valid_date_string)
+        expect(instance).to be_valid
+      end
     end
-  end
 
-  it 'validates MM/DD/YY date strings' do
-    ['9/10/18', '8/1/18', '2/28/17', '3/28/19'].each do |valid_date_string|
-      instance = sheet_class.new_from_params('An Ingested Date' => valid_date_string)
-      expect(instance).to be_valid
+    it 'validates MM/DD/YY date strings' do
+      ['9/10/18', '8/1/18', '2/28/17', '3/28/19'].each do |valid_date_string|
+        instance = sheet_class.new_from_params('An Ingested Date' => valid_date_string)
+        expect(instance).to be_valid
+      end
     end
-  end
 
-  it 'does not validate correctly formatted but invalid dates' do
-    ['28/9/17', '9/21/2017'].each do |invalid_date_string|
-      instance = sheet_class.new_from_params('An Ingested Date' => invalid_date_string)
+    it 'does not validate correctly formatted but invalid dates' do
+      ['28/9/17', '9/21/2017'].each do |invalid_date_string|
+        instance = sheet_class.new_from_params('An Ingested Date' => invalid_date_string)
+        expect(instance).not_to be_valid
+        expect(instance.errors['An Ingested Date'].first).to eq 'must be in the format dd/mm/yyyy'
+      end
+    end
+
+    it 'does not validate values not formatted as a date' do
+      instance = sheet_class.new_from_params('An Ingested Date' => 'Bob')
       expect(instance).not_to be_valid
       expect(instance.errors['An Ingested Date'].first).to eq 'must be in the format dd/mm/yyyy'
     end
   end
 
-  it 'does not validate values not formatted as a date' do
-    instance = sheet_class.new_from_params('An Ingested Date' => 'Bob')
-    expect(instance).not_to be_valid
-    expect(instance.errors['An Ingested Date'].first).to eq 'must be in the format dd/mm/yyyy'
+  context 'validating an optional field' do
+    let(:sheet_class) do
+      Class.new(Framework::Sheet) do
+        extend ActiveModel::Naming
+
+        def self.name
+          'Validator'
+        end
+
+        field 'Optional Ingested Date', :string, ingested_date: true, allow_nil: true
+      end
+    end
+
+    it 'validates a correctly-formatted date string' do
+      instance = sheet_class.new_from_params('Optional Ingested Date' => '12/10/2018')
+      expect(instance).to be_valid
+    end
+
+    it 'validates a blank value' do
+      instance = sheet_class.new_from_params('Optional Ingested Date' => nil)
+      expect(instance).to be_valid
+    end
+
+    it 'mentions that the field is optional in the error message' do
+      instance = sheet_class.new_from_params('Optional Ingested Date' => 'Bad Date')
+      expect(instance).not_to be_valid
+      expect(instance.errors['Optional Ingested Date'].first).to eq 'must be in the format dd/mm/yyyy or left blank'
+    end
   end
 end
