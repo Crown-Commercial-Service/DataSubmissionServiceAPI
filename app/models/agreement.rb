d947629e3d5931518d4bd2460b08e6b5ff0aa24d
Tekin Suleyman <tekin@tekin.co.uk>
2018-12-11 10:19:13 +0000

    Ensure duplicate agreements cannot be created
    Now that we are about to model and validate against lots, we need to
    should make sure each supplier has a single unique agreement against any
    given framework to avoid any complications or errors. This may change in
    the future when we introduce the dynamic frameworks that suppliers
    can join and leave at will. At that point we'll likely introduce
    start/end dates for agreements and this can switch to validating there
    is no overlap in dates instead.

--- app/models/agreement.rb
+++ app/models/agreement.rb
@@ -5,5 +5,6 @@ class Agreement < ApplicationRecord
   has_many :framework_lots, through: :agreement_framework_lots
 
   validates :supplier_id, presence: true
-  validates :framework_id, presence: true
+  validates :framework_id, presence: true,
+    uniqueness: { scope: :supplier_id, message: 'agreement already exists for this supplier' }
 end
--- spec/models/agreement_spec.rb
+++ spec/models/agreement_spec.rb
@@ -1,4 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Agreement do
+  it 'does not allow a supplier to have more than one agreement against the same framework' do
+    agreement = FactoryBot.create(:agreement)
+
+    duplicate_agreement = agreement.supplier.agreements.new(framework: agreement.framework)
+
+    expect(duplicate_agreement).not_to be_valid
+  end
 end
