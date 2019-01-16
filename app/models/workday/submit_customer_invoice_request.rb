ad4630da9ad6a5c99cbaae185cf2db408d32b497
Tekin Suleyman <tekin@tekin.co.uk>
2019-01-16 10:53:50 +0000

    Introduce Workday::SubmitCustomerInvoiceRequest
    This generates the SOAP API request payload to create an invoice in
    Workday for a given submission.
    
    The Workday API requires that a revenue category and cost center is
    specified in the requests. However we do not currently have a mechanism
    to identify these, so they are hardcoded for the time being. A new API
    endpoint is being added to Workday that will allow us to map a framework
    to its revenue category and cost center.
    
    Note that the authentication headers are not yet included, those will
    come in a subsequent commit.

---
+++ app/models/workday/submit_customer_invoice_request.rb
@@ -0,0 +1,90 @@
+require 'lolsoap'
+
+module Workday
+  CCS_COMPANY_REFERENCE = 'Crown_Commercial_Service'.freeze
+
+  class SubmitCustomerInvoiceRequest
+    def initialize(submission)
+      @submission = submission
+      @client = LolSoap::Client.new(File.read('data/workday_revenue_management_v31.0.xml'))
+      @request = @client.request('Submit_Customer_Invoice')
+
+      prepare_request_body
+    end
+
+    delegate :url, :content, to: :request
+
+    private
+
+    attr_reader :request, :submission
+
+    # rubocop:disable Metrics/AbcSize
+    def prepare_request_body
+      @request.body do |body|
+        body.Customer_Invoice_Data do |invoice|
+          invoice.Company_Reference.ID    CCS_COMPANY_REFERENCE, 'ns0:type': 'Company_Reference_ID'
+          invoice.Customer_Reference.ID   supplier_salesforce_id, 'ns0:type': 'Customer_Reference_ID'
+          invoice.From_Date               task_period_date.to_s
+          invoice.Customer_PO_Number      submission.purchase_order_number
+          invoice.Memo                    invoice_memo
+
+          invoice.Customer_Invoice_Line_Replacement_Data do |invoice_line|
+            invoice_line.Line_Item_Description      line_item_description
+            invoice_line.Extended_Amount            management_charge
+            invoice_line.Analytical_Amount          total_spend
+            invoice_line.Revenue_Category_Reference.ID framework_revenue_category_id, 'ns0:type': 'WID'
+            invoice_line.Worktags_Reference.ID      framework_cost_center_id, 'ns0:type': 'WID'
+            invoice_line.Worktags_Reference.ID      framework.short_name, 'ns0:type': 'Custom_Organization_Reference_ID'
+          end
+        end
+      end
+    end
+    # rubocop:enable Metrics/AbcSize
+
+    def framework
+      submission.framework
+    end
+
+    # NOTE: Hardcoded until we have access to the endpoint to identify this ID in Workday
+    def framework_cost_center_id
+      'e02956ef5ea301814e48cb47a267e959'
+    end
+
+    # NOTE: Hardcoded until we have access to the endpoint to identify this ID in Workday
+    def framework_revenue_category_id
+      'a2f06ac13aa001298fa4baeb696a06f3'
+    end
+
+    def invoice_memo
+      "Submission ID: #{submission.id}"
+    end
+
+    def line_item_description
+      "Management charge for #{task_period_in_words} based on £#{total_spend} spend"
+    end
+
+    def management_charge
+      format '%.2f', submission.entries.invoices.sum(:management_charge).truncate(2)
+    end
+
+    def total_spend
+      format '%.2f', submission.entries.invoices.sum(:total_value).truncate(2)
+    end
+
+    def supplier_salesforce_id
+      submission.supplier.salesforce_id
+    end
+
+    def task
+      submission.task
+    end
+
+    def task_period_date
+      Date.new(task.period_year, task.period_month)
+    end
+
+    def task_period_in_words
+      task_period_date.strftime('%B %Y')
+    end
+  end
+end
---
+++ spec/models/workday/submit_customer_invoice_request_spec.rb
@@ -0,0 +1,79 @@
+require 'rails_helper'
+
+RSpec.describe Workday::SubmitCustomerInvoiceRequest do
+  let(:submission) { FactoryBot.create(:submission_with_validated_entries, purchase_order_number: '123') }
+  let(:framework) { submission.framework }
+  let(:task) { FactoryBot.create(:task, period_month: 12, period_year: 2018) }
+  let(:supplier) { submission.supplier }
+  let(:request) { Workday::SubmitCustomerInvoiceRequest.new(submission) }
+
+  describe '#url' do
+    it 'returns the URL for the Workday Revenue Management web service' do
+      expect(request.url).to eq 'https://wd3-impl-services1.workday.com/ccx/service/crowncommercialservice4/Revenue_Management/v31.0'
+    end
+  end
+
+  describe '#content' do
+    it 'sets Company_Reference with the CCS Workday Company_Reference_ID' do
+      expect(
+        text_at_xpath("//ns0:Company_Reference//ns0:ID[@ns0:type='Company_Reference_ID']")
+      ).to eq 'Crown_Commercial_Service'
+    end
+
+    it 'sets Customer_Reference with the Supplier’s salesforce_id identified as Customer_Reference_ID' do
+      expect(
+        text_at_xpath("//ns0:Customer_Reference//ns0:ID[@ns0:type='Customer_Reference_ID']")
+      ).to eq supplier.salesforce_id
+    end
+
+    it 'sets From_Date with the date the Submission is being made against' do
+      expect(text_at_xpath('//ns0:From_Date')).to eq Date.new(task.period_year, task.period_month).to_s
+    end
+
+    it 'sets Customer_PO_Number with the Submission’s purchase order number' do
+      expect(text_at_xpath('//ns0:Customer_PO_Number')).to eq submission.purchase_order_number
+    end
+
+    it 'sets Memo with the Submission’s ID' do
+      expect(text_at_xpath('//ns0:Memo')).to eq "Submission ID: #{submission.id}"
+    end
+
+    describe '//Customer_Invoice_Line_Replacement_Data' do
+      it 'sets Line_Item_Description with a description of the charge' do
+        expect(
+          text_at_xpath('//ns0:Customer_Invoice_Line_Replacement_Data//ns0:Line_Item_Description')
+        ).to eq 'Management charge for December 2018 based on £20.00 spend'
+      end
+
+      it 'sets Analytical_Amount as the total spend for the submission' do
+        expect(text_at_xpath('//ns0:Customer_Invoice_Line_Replacement_Data//ns0:Analytical_Amount')).to eq '20.00'
+      end
+
+      it 'sets Extended_Amount as the management charge for the submission' do
+        expect(text_at_xpath('//ns0:Customer_Invoice_Line_Replacement_Data//ns0:Extended_Amount')).to eq '0.20'
+      end
+
+      it 'sets a Worktags_Reference//ID with the framework short name identified as Custom_Organization_Reference_ID' do
+        expect(
+          text_at_xpath("//ns0:Worktags_Reference//ns0:ID[@ns0:type='Custom_Organization_Reference_ID']")
+        ).to eq framework.short_name
+      end
+
+      pending 'sets Revenue_Category_Reference//ID as the revenue category Worday ID for the Framework' do
+        expect(
+          text_at_xpath("//ns0:Revenue_Category_Reference//ns0:ID[@ns0:type='WID']")
+        ).to eq 'A revenue category ID from workday'
+      end
+
+      pending 'sets a Worktags_Reference//ID with the cost center Workday ID for the Framework' do
+        expect(
+          text_at_xpath("//ns0:Worktags_Reference//ns0:ID[@ns0:type='WID']")
+        ).to eq 'A cost center ID from Workday'
+      end
+    end
+
+    def text_at_xpath(xpath)
+      Nokogiri::XML(request.content).at_xpath(xpath).text
+    end
+  end
+end
