55aef7a935622dba770ece8898568069e4c3b994
James Darling <james@abscond.org>
2019-01-28 14:04:33 +0000

    Submit InvoiceAdjustment
    instead of Invoice if management charge is negative

--- app/jobs/submission_invoice_creation_job.rb
+++ app/jobs/submission_invoice_creation_job.rb
@@ -2,7 +2,11 @@ class SubmissionInvoiceCreationJob < ApplicationJob
   def perform(submission)
     raise "Submission #{submission.id} already has an invoice." if submission.invoice.present?
 
-    workday_reference = Workday::SubmitCustomerInvoiceRequest.new(submission).perform
+    workday_reference = if submission.management_charge.negative?
+                          Workday::SubmitCustomerInvoiceAdjustmentRequest.new(submission).perform
+                        else
+                          Workday::SubmitCustomerInvoiceRequest.new(submission).perform
+                        end
     submission.create_invoice!(workday_reference: workday_reference)
   end
 end
