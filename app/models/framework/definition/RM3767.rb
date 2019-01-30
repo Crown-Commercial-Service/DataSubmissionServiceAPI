ca1867819244e4ddb4ca76bc164b86bb5d8d91a7
Tekin Suleyman <tekin@tekin.co.uk>
2019-01-30 12:26:19 +0000

    Reorder RM3767 definition fields
    Re-order the field definitions to match their order in the framework
    template to a) make it easier to verify against the template, and b)
    make it easier in the future to generate a template that matches what we
    already have based on the definition.

--- app/models/framework/definition/RM3767.rb
+++ app/models/framework/definition/RM3767.rb
@@ -11,26 +11,26 @@ class Framework
 
         field 'Lot Number', :string, exports_to: 'LotNumber', presence: true, lot_in_agreement: true
         field 'Customer URN', :integer, exports_to: 'CustomerURN', urn: true
-        field 'Vehicle Category', :string, exports_to: 'ProductSubClass'
-        field 'Associated Service', :string, exports_to: 'ProductDescription'
-        field 'Tyre Width', :string
-        field 'Aspect Ratio', :string
-        field 'Rim Diameter', :string
-        field 'Load Capacity', :string
-        field 'Speed Index', :string
         field 'Customer Organisation Name', :string, exports_to: 'CustomerName', presence: true
         field 'Customer Invoice Date', :string, exports_to: 'InvoiceDate', ingested_date: true
         field 'Customer Invoice Number', :string, exports_to: 'InvoiceNumber'
         field 'Product Type', :string, exports_to: 'ProductGroup'
         field 'Tyre Brand', :string, exports_to: 'ProductClass'
+        field 'Tyre Width', :string
+        field 'Aspect Ratio', :string
+        field 'Rim Diameter', :string
+        field 'Load Capacity', :string
+        field 'Speed Index', :string
+        field 'Vehicle Category', :string, exports_to: 'ProductSubClass'
+        field 'Tyre Grade', :string, exports_to: 'Additional1'
+        field 'Associated Service', :string, exports_to: 'ProductDescription'
+        field 'Run Flats (Y/N)', :string, exports_to: 'Additional2', presence: true, case_insensitive_inclusion: { in: %w[Y N], message: "must be 'Y' or 'N'" }
         field 'UNSPSC', :string, exports_to: 'UNSPSC', ingested_numericality: { only_integer: true }
         field 'Unit of Purchase', :string, exports_to: 'UnitType'
         field 'Price per Unit', :string, exports_to: 'UnitPrice', ingested_numericality: true, allow_nil: true
-        field 'Total Cost (ex VAT)', :string, exports_to: 'InvoiceValue', ingested_numericality: true
         field 'Quantity', :string, exports_to: 'UnitQuantity', ingested_numericality: true, allow_nil: true
+        field 'Total Cost (ex VAT)', :string, exports_to: 'InvoiceValue', ingested_numericality: true
         field 'VAT Amount Charged', :string, exports_to: 'VATCharged', ingested_numericality: true, allow_nil: true
-        field 'Tyre Grade', :string, exports_to: 'Additional1'
-        field 'Run Flats (Y/N)', :string, exports_to: 'Additional2', presence: true, case_insensitive_inclusion: { in: %w[Y N], message: "must be 'Y' or 'N'" }
         field 'Subcontractor Name', :string, exports_to: 'Additional8'
       end
     end
