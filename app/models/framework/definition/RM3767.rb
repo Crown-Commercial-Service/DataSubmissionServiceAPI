17f400aedd4a187f91fa37327b7c452821605170
Tekin Suleyman <tekin@tekin.co.uk>
2019-01-30 16:05:23 +0000

    Remove redundant fields from RM3767 definition
    These fields are not actually present in the Framework template.

--- app/models/framework/definition/RM3767.rb
+++ app/models/framework/definition/RM3767.rb
@@ -18,11 +18,9 @@ class Framework
         field 'Rim Diameter', :string
         field 'Load Capacity', :string
         field 'Speed Index', :string
-        field 'Customer PostCode', :string, exports_to: 'CustomerPostCode'
         field 'Customer Organisation Name', :string, exports_to: 'CustomerName', presence: true
         field 'Customer Invoice Date', :string, exports_to: 'InvoiceDate', ingested_date: true
         field 'Customer Invoice Number', :string, exports_to: 'InvoiceNumber'
-        field 'Customer Invoice Line Number', :string
         field 'Product Type', :string, exports_to: 'ProductGroup'
         field 'Tyre Brand', :string, exports_to: 'ProductClass'
         field 'UNSPSC', :string, exports_to: 'UNSPSC', ingested_numericality: { only_integer: true }
@@ -31,8 +29,6 @@ class Framework
         field 'Total Cost (ex VAT)', :string, exports_to: 'InvoiceValue', ingested_numericality: true
         field 'Quantity', :string, exports_to: 'UnitQuantity', ingested_numericality: true, allow_nil: true
         field 'VAT Amount Charged', :string, exports_to: 'VATCharged', ingested_numericality: true, allow_nil: true
-        field 'Cost Centre', :string
-        field 'Contract Number', :string
         field 'Tyre Grade', :string, exports_to: 'Additional1'
         field 'Run Flats (Y/N)', :string, exports_to: 'Additional2', presence: true, case_insensitive_inclusion: { in: %w[Y N], message: "must be 'Y' or 'N'" }
         field 'Subcontractor Name', :string, exports_to: 'Additional8'
