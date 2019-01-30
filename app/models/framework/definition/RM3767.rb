ddb61684002b857212fc7ce04e1d7ce619e4fda9
Tekin Suleyman <tekin@tekin.co.uk>
2019-01-30 15:56:45 +0000

    Re-instate validations for RM3767
    The validations for this framework were slackened before the framework
    went live in October because it was discovered that suppliers were
    already submitting data with invalid data.
    
    In collaboration with Malcolm Eastoe from the MI Team, we are now
    re-instated some essential validations that will not cause major
    failures for suppliers.
    
    A small number of existing submissions do fail with these changes, but
    not enough to cause concern for suppliers making valid submissions in
    the future:
    
      49493aa4-ff1c-4697-a2d6-7c6b38fa8304
      f6a5e94d-20e3-41c3-b81a-c9f750063332
      63b16d3b-cdba-4d81-96a0-8c4108c775aa
      45afb85e-0537-4c54-acbf-8882f0b37654
      5a3e8239-5c69-4455-91c5-4b5572ab2e90
      d80236ef-816d-49b1-b337-2e7365afb662
      db31f1b9-b12c-4c19-b116-0ec298e01bee

--- app/models/framework/definition/RM3767.rb
+++ app/models/framework/definition/RM3767.rb
@@ -6,6 +6,27 @@ class Framework
 
       management_charge_rate BigDecimal('1')
 
+      PRODUCT_TYPES = [
+        'Tyre - Supply ONLY',
+        'Tyre - Supply + Fit',
+        'Casing Credit'
+      ].freeze
+
+      VEHICLE_CATEGORY = [
+        'Car',
+        '4x4',
+        'Van',
+        'Truck',
+        'Motorcycle',
+        'Agrarian'
+      ].freeze
+
+      TYRE_GRADE = [
+        'Premium',
+        'Mid-Range',
+        'Budget'
+      ].freeze
+
       class Invoice < EntryData
         total_value_field 'Total Cost (ex VAT)'
 
@@ -13,24 +34,24 @@ class Framework
         field 'Customer URN', :integer, exports_to: 'CustomerURN', urn: true
         field 'Customer Organisation Name', :string, exports_to: 'CustomerName', presence: true
         field 'Customer Invoice Date', :string, exports_to: 'InvoiceDate', ingested_date: true
-        field 'Customer Invoice Number', :string, exports_to: 'InvoiceNumber'
-        field 'Product Type', :string, exports_to: 'ProductGroup'
-        field 'Tyre Brand', :string, exports_to: 'ProductClass'
-        field 'Tyre Width', :string
-        field 'Aspect Ratio', :string
-        field 'Rim Diameter', :string
+        field 'Customer Invoice Number', :string, exports_to: 'InvoiceNumber', presence: true
+        field 'Product Type', :string, exports_to: 'ProductGroup', case_insensitive_inclusion: { in: PRODUCT_TYPES }
+        field 'Tyre Brand', :string, exports_to: 'ProductClass', presence: true
+        field 'Tyre Width', :string, presence: true
+        field 'Aspect Ratio', :string, presence: true
+        field 'Rim Diameter', :string, presence: true
         field 'Load Capacity', :string
         field 'Speed Index', :string
-        field 'Vehicle Category', :string, exports_to: 'ProductSubClass'
-        field 'Tyre Grade', :string, exports_to: 'Additional1'
-        field 'Associated Service', :string, exports_to: 'ProductDescription'
+        field 'Vehicle Category', :string, exports_to: 'ProductSubClass', case_insensitive_inclusion: { in: VEHICLE_CATEGORY }
+        field 'Tyre Grade', :string, exports_to: 'Additional1', case_insensitive_inclusion: { in: TYRE_GRADE }
+        field 'Associated Service', :string, exports_to: 'ProductDescription', presence: true
         field 'Run Flats (Y/N)', :string, exports_to: 'Additional2', presence: true, case_insensitive_inclusion: { in: %w[Y N], message: "must be 'Y' or 'N'" }
         field 'UNSPSC', :string, exports_to: 'UNSPSC', ingested_numericality: { only_integer: true }
         field 'Unit of Purchase', :string, exports_to: 'UnitType'
-        field 'Price per Unit', :string, exports_to: 'UnitPrice', ingested_numericality: true, allow_nil: true
-        field 'Quantity', :string, exports_to: 'UnitQuantity', ingested_numericality: true, allow_nil: true
+        field 'Price per Unit', :string, exports_to: 'UnitPrice', ingested_numericality: true
+        field 'Quantity', :string, exports_to: 'UnitQuantity', ingested_numericality: true
         field 'Total Cost (ex VAT)', :string, exports_to: 'InvoiceValue', ingested_numericality: true
-        field 'VAT Amount Charged', :string, exports_to: 'VATCharged', ingested_numericality: true, allow_nil: true
+        field 'VAT Amount Charged', :string, exports_to: 'VATCharged', ingested_numericality: true
         field 'Subcontractor Name', :string, exports_to: 'Additional8'
       end
     end
