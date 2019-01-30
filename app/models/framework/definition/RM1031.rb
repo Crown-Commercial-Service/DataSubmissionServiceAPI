f57d1df25a99463e43faf50089e7c2cbe9bace98
Tekin Suleyman <tekin@tekin.co.uk>
2019-01-30 12:21:22 +0000

    Re-instate validations for RM1031
    The validations for this framework were slackened before the framework
    went live in October because it was discovered that suppliers were
    already submitting data with invalid data.
    
    In collaboration with Malcolm Eastoe from the MI Team, we are now
    re-instated some essential validations that will not cause major
    failures for suppliers.
    
    A small number of existing submissions do fail with these changes,
    all for Berendsen UK Limited, which isn't enough to cause concern for
    suppliers making valid submissions in the future:
    
        eae7f01d-581a-4e37-bdfe-9d8f3cbcb956
        639b4ffa-d42b-4d47-a358-f86d56a135f3
        e8a6b9f2-169f-4db2-a336-11e58e112646

--- app/models/framework/definition/RM1031.rb
+++ app/models/framework/definition/RM1031.rb
@@ -14,17 +14,17 @@ class Framework
         field 'Customer URN', :integer, exports_to: 'CustomerURN', urn: true
         field 'Customer Invoice Date', :string, exports_to: 'InvoiceDate', ingested_date: true
         field 'Customer Invoice Number', :string, exports_to: 'InvoiceNumber', presence: true
-        field 'Service Type', :string, exports_to: 'ProductGroup'
-        field 'Category', :string, exports_to: 'ProductClass'
-        field 'Item', :string, exports_to: 'ProductDescription'
-        field 'Item Code', :string, exports_to: 'ProductCode'
-        field 'UNSPSC', :string, exports_to: 'UNSPSC', ingested_numericality: { only_integer: true }, allow_nil: true
+        field 'Service Type', :string, exports_to: 'ProductGroup', presence: true
+        field 'Category', :string, exports_to: 'ProductClass', presence: true
+        field 'Item', :string, exports_to: 'ProductDescription', presence: true
+        field 'Item Code', :string, exports_to: 'ProductCode', presence: true
+        field 'UNSPSC', :string, exports_to: 'UNSPSC', ingested_numericality: { only_integer: true }
         field 'Baseline Price', :string, exports_to: 'Additional1', ingested_numericality: true, allow_nil: true
-        field 'Unit of Purchase', :string, exports_to: 'UnitType'
-        field 'Price per Unit', :string, exports_to: 'UnitPrice', ingested_numericality: true, allow_nil: true
-        field 'Quantity', :string, exports_to: 'UnitQuantity', ingested_numericality: true, allow_nil: true
+        field 'Unit of Purchase', :string, exports_to: 'UnitType', presence: true
+        field 'Price per Unit', :string, exports_to: 'UnitPrice', ingested_numericality: true
+        field 'Quantity', :string, exports_to: 'UnitQuantity', ingested_numericality: true
         field 'Total Charge (Ex VAT)', :string, exports_to: 'InvoiceValue', ingested_numericality: true
-        field 'VAT amount charged', :string, exports_to: 'VATCharged', ingested_numericality: true, allow_nil: true
+        field 'VAT amount charged', :string, exports_to: 'VATCharged', ingested_numericality: true
         field 'Subcontractor Supplier Name', :string, exports_to: 'Additional2'
         field 'Cost Centre', :string
         field 'Contract Number', :string
@@ -37,8 +37,8 @@ class Framework
         field 'Customer PostCode', :string, exports_to: 'CustomerPostCode'
         field 'Customer URN', :integer, exports_to: 'CustomerURN', urn: true
         field 'Project Name', :string, exports_to: 'ProductDescription'
-        field 'Number of items', :string, ingested_numericality: true, allow_nil: true
-        field 'Customer Order/Contract Value', :string, exports_to: 'ContractValue', ingested_numericality: true, allow_nil: true
+        field 'Number of items', :string, ingested_numericality: true
+        field 'Customer Order/Contract Value', :string, exports_to: 'ContractValue', ingested_numericality: true
       end
     end
   end
