ae63171b3686253cfe3c5a13cbd0416c56f00aab
Cristina <cristina@dxw.com>
2019-02-04 17:27:05 +0000

    Reorder RM807 definition fields
    Re-order the field definitions to match their order in the framework
    template to a) make it easier to verify against the template, and b)
    make it easier in the future to generate a template that matches what we
    already have based on the definition.

--- app/models/framework/definition/RM807.rb
+++ app/models/framework/definition/RM807.rb
@@ -9,13 +9,6 @@ class Framework
       class Invoice < EntryData
         total_value_field 'Total Charges (ex VAT)'
 
-        field 'Contract Number', :string
-        field 'Cost Centre', :string
-        field 'Miles Travelled', :string, exports_to: 'Additional6', allow_nil: true, ingested_numericality: true
-        field 'Refuelling Charges inc fuel ex VAT', :string, exports_to: 'Additional7', allow_nil: true, ingested_numericality: true
-        field 'Rental Company', :string, exports_to: 'Additional8'
-        field 'Booking Method', :string
-        field 'On line booking discount', :string, allow_nil: true, ingested_numericality: true
         field 'Lot Number', :string, exports_to: 'LotNumber', presence: true, lot_in_agreement: true
         field 'Customer PostCode', :string, exports_to: 'CustomerPostCode'
         field 'Customer Organisation', :string, exports_to: 'CustomerName', presence: true
@@ -25,6 +18,7 @@ class Framework
         field 'Customer Invoice Number', :string, exports_to: 'InvoiceNumber', presence: true
         field 'Customer Invoice Line Number', :string
         field 'Invoice Line Product/Service Description', :string, exports_to: 'ProductDescription'
+        field 'Invoice Line Product/Service Grouping', :string, exports_to: 'ProductGroup'
         field 'UNSPSC', :string, exports_to: 'UNSPSC', allow_nil: true, ingested_numericality: { only_integer: true }
         field 'Unit of Purchase', :string, exports_to: 'UnitType'
         field 'Price per Unit', :string, exports_to: 'UnitPrice', ingested_numericality: true
@@ -33,7 +27,6 @@ class Framework
         field 'VAT Applicable', :string, exports_to: 'VATIncluded', presence: true, case_insensitive_inclusion: { in: %w[Y N], message: "must be 'Y' or 'N'" }
         field 'VAT amount charged', :string, exports_to: 'VATCharged', ingested_numericality: true, allow_nil: true
         field 'Promotion Code', :string, exports_to: 'PromotionCode'
-        field 'Invoice Line Product/Service Grouping', :string, exports_to: 'ProductGroup'
         field 'Rental Period', :string, exports_to: 'Additional1'
         field 'Vehicle Group Booked', :string, exports_to: 'Additional2'
         field 'Vehicle Group Supplied', :string, exports_to: 'Additional3'
@@ -41,6 +34,13 @@ class Framework
         field 'Vehicle Derivative', :string, exports_to: 'ProductSubClass'
         field 'Fuel Type', :string, exports_to: 'Additional4'
         field 'CO2 Emission Level', :string, exports_to: 'Additional5'
+        field 'Miles Travelled', :string, exports_to: 'Additional6', allow_nil: true, ingested_numericality: true
+        field 'Refuelling Charges inc fuel ex VAT', :string, exports_to: 'Additional7', allow_nil: true, ingested_numericality: true
+        field 'Rental Company', :string, exports_to: 'Additional8'
+        field 'Booking Method', :string
+        field 'On line booking discount', :string, allow_nil: true, ingested_numericality: true
+        field 'Cost Centre', :string
+        field 'Contract Number', :string
       end
     end
   end
