3318cd9830851771be7427edf972d7ad8bc97d65
Cristina <cristina@dxw.com>
2019-02-04 17:30:15 +0000

    Reinstate some validations for RM807
    Similar to RM3710, according to Malcolm Eastoe in a call on Mon 4 Feb 2019.
    
    There are 8 submissions that would fail validation under the
    newly added rules:
    `5cee9d12-24bf-4d43-8a45-976baa927f38`
    `c5aa536c-b44d-4052-a412-2ecffcd91f72`
    `844c9ba0-e327-45e8-8377-dfdbf7464a6f`
    `e613d9d8-3f9a-4cbf-a439-1374f055eaaa`
    `e1b7cbfe-7ab9-43cb-af78-7f9726ae9895`
    `622ce783-962e-4b5c-a5b1-432fd32e4f79`
    `7c3d750a-2939-4771-afee-9e3165e41c69`
    `dfb7a393-ecbd-441f-9a5c-a51f58a265f2`
    
    This has been deemed acceptale.

--- app/models/framework/definition/RM807.rb
+++ app/models/framework/definition/RM807.rb
@@ -20,19 +20,19 @@ class Framework
         field 'Invoice Line Product/Service Description', :string, exports_to: 'ProductDescription'
         field 'Invoice Line Product/Service Grouping', :string, exports_to: 'ProductGroup'
         field 'UNSPSC', :string, exports_to: 'UNSPSC', allow_nil: true, ingested_numericality: { only_integer: true }
-        field 'Unit of Purchase', :string, exports_to: 'UnitType'
+        field 'Unit of Purchase', :string, exports_to: 'UnitType', presence: true
         field 'Price per Unit', :string, exports_to: 'UnitPrice', ingested_numericality: true
         field 'Invoice Line Quantity', :string, exports_to: 'UnitQuantity', ingested_numericality: true
         field 'Total Charges (ex VAT)', :string, exports_to: 'InvoiceValue', ingested_numericality: true
         field 'VAT Applicable', :string, exports_to: 'VATIncluded', presence: true, case_insensitive_inclusion: { in: %w[Y N], message: "must be 'Y' or 'N'" }
-        field 'VAT amount charged', :string, exports_to: 'VATCharged', ingested_numericality: true, allow_nil: true
+        field 'VAT amount charged', :string, exports_to: 'VATCharged', ingested_numericality: true
         field 'Promotion Code', :string, exports_to: 'PromotionCode'
         field 'Rental Period', :string, exports_to: 'Additional1'
         field 'Vehicle Group Booked', :string, exports_to: 'Additional2'
         field 'Vehicle Group Supplied', :string, exports_to: 'Additional3'
-        field 'Vehicle Make', :string, exports_to: 'ProductClass'
-        field 'Vehicle Derivative', :string, exports_to: 'ProductSubClass'
-        field 'Fuel Type', :string, exports_to: 'Additional4'
+        field 'Vehicle Make', :string, exports_to: 'ProductClass', presence: true
+        field 'Vehicle Derivative', :string, exports_to: 'ProductSubClass', presence: true
+        field 'Fuel Type', :string, exports_to: 'Additional4', presence: true
         field 'CO2 Emission Level', :string, exports_to: 'Additional5'
         field 'Miles Travelled', :string, exports_to: 'Additional6', allow_nil: true, ingested_numericality: true
         field 'Refuelling Charges inc fuel ex VAT', :string, exports_to: 'Additional7', allow_nil: true, ingested_numericality: true
