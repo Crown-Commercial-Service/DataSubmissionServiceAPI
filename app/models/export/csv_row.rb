a6068c007d07163e22749790c5da9b36b668b850
Russell Garner <rgarner@zephyros-systems.co.uk>
2019-02-11 13:40:07 +0000

    Remove MISSING/BLANK_FOR_NOW from CsvRow
    These were used when getting to MVP. They aren't needed now.

--- app/models/export/csv_row.rb
+++ app/models/export/csv_row.rb
@@ -1,8 +1,6 @@
 module Export
   class CsvRow
-    MISSING = '#MISSING'.freeze       # fields that are needed for MVP that we don't have yet
     NOT_IN_DATA = '#NOTINDATA'.freeze # fields that we looked for in the JSONB data and could not find
-    BLANK_FOR_NOW = nil               # things that can be blank for MVP
 
     attr_reader :model
 
