3f0cbeded4e16b3c9c20e4c0ff81374ee07d7b0e
Russell Garner <rgarner@zephyros-systems.co.uk>
2019-03-04 10:03:40 +0000

    Add a task factory trait `:current`
    Be explicit in factory comments about why tasks are created late (it's
    a safe default). Offer a trait that creates a current task should one
    be needed.

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -1,12 +1,26 @@
 FactoryBot.define do
+  # To be a safe default and to avoid specs which become time bombs,
+  # tasks are created with fixed dates, which makes them late.
+  #
+  # Use +create :task, :current+ if you wish to create a task
+  # within the current window and will make your spec match this.
+  #
+  # https://github.com/dxw/DataSubmissionServiceAPI/pull/290
   factory :task do
     status 'unstarted'
+
     period_month 1
     period_year  2019
     due_on       Date.strptime('2019-02-28').beginning_of_month + 7.days
 
     supplier
     framework
+
+    trait :current do
+      period_month { Time.zone.today.last_month.month }
+      period_year  { Time.zone.today.last_month.year }
+      due_on       { Time.zone.today.beginning_of_month + 7.days }
+    end
 
     trait :completed do
       status 'completed'
