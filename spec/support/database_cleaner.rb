f5d8fee706cd0380e38fdb42aa51446dc0379e29
Tekin Suleyman <tekin@tekin.co.uk>
2019-03-13 15:09:59 +0000

    Update database cleaner/transactional fixture conf
    Transactional fixtures were enabled in rails_helper.rb, which
    contradicted the config in support/database_cleaner.rb. Update the
    config there to avoid any ambiguity and ensure transactional fixtures
    are definitely disabled.
    
    This commit also updates the DatabaseCleaner config to match the
    suggested configuration in the gem's README:
    
      https://github.com/DatabaseCleaner/database_cleaner#rspec-with-capybara-example
    
    Note that DatabaseCleaner.allow_remote_database_url is set to true so
    that the test suite can run on the CI server.

--- spec/support/database_cleaner.rb
+++ spec/support/database_cleaner.rb
@@ -1,7 +1,10 @@
+require 'capybara/rspec'
+
 RSpec.configure do |config|
   DatabaseCleaner.allow_remote_database_url = true
 
   config.use_transactional_fixtures = false
+
   config.before(:suite) do
     DatabaseCleaner.clean_with(:truncation)
   end
@@ -10,11 +13,24 @@ RSpec.configure do |config|
     DatabaseCleaner.strategy = :transaction
   end
 
+  config.before(:each, type: :feature) do
+    # :rack_test driver's Rack app under test shares database connection
+    # with the specs, so continue to use transaction strategy for speed.
+    driver_shares_db_connection_with_specs = Capybara.current_driver == :rack_test
+
+    unless driver_shares_db_connection_with_specs
+      # Driver is probably for an external browser with an app
+      # under test that does *not* share a database connection with the
+      # specs, so use truncation strategy.
+      DatabaseCleaner.strategy = :truncation
+    end
+  end
+
   config.before(:each) do
     DatabaseCleaner.start
   end
 
-  config.after(:each) do
+  config.append_after(:each) do
     DatabaseCleaner.clean
   end
 end
