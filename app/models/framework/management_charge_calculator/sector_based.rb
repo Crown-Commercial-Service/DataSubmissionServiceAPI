e59500f92f18d711c5f725d2ce4e3e0db2b145e4
Laura Porter <laura@dxw.com>
2019-03-18 15:45:52 +0000

    Implement sector-based management charge
    Framework::ManagementChargeCalculator::SectorBased calculates the
    management charge for a submission entry dependent on whether the customer is
    central government or the wider public sector. This is determined from the
    customer's URN.

---
+++ app/models/framework/management_charge_calculator/sector_based.rb
@@ -0,0 +1,24 @@
+class Framework
+  module ManagementChargeCalculator
+    class SectorBased
+      attr_reader :central_government, :wider_public_sector
+
+      def initialize(central_government:, wider_public_sector:)
+        @central_government = central_government
+        @wider_public_sector = wider_public_sector
+      end
+
+      def calculate_for(entry)
+        (entry.total_value * (percentage(entry) / 100)).truncate(4)
+      end
+
+      private
+
+      def percentage(entry)
+        urn = entry.customer_urn
+        sector = Customer.select(:sector).find_by(urn: urn).sector
+        send(sector)
+      end
+    end
+  end
+end
