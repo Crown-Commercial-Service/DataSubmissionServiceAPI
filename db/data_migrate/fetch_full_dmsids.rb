3425e2ad0e90a974731b5aa5502b16082ce2075c
Robert Lee-Cann <leeky@leeky.org.uk>
2019-04-04 14:13:37 +0100

    Generate data_migration CSV

--- db/data_migrate/fetch_full_dmsids.rb
+++ db/data_migrate/fetch_full_dmsids.rb
@@ -1,5 +1,6 @@
 require 'roo'
 require 'roo-xls'
+require 'csv'
 
 submission_ids = SubmissionEntry
                  .where("LENGTH(data->>'Digital Marketplace Service ID') = 4")
@@ -7,15 +8,15 @@ submission_ids = SubmissionEntry
                  .pluck(:submission_id)
                  .uniq
 
-# submission_ids = Array(submission_ids.first) # for testing
+entries_to_correct = {}
 
 submission_ids.each do |submission_id|
   submission = Submission.find(submission_id)
   file = submission.files&.first
   next if file.nil?
 
-  invoice_entries = submission.entries.invoices.order("CAST(source->>'row' AS INTEGER)")
-  order_entries = submission.entries.orders.order("CAST(source->>'row' AS INTEGER)")
+  invoice_entries = submission.entries.invoices
+  order_entries = submission.entries.orders
 
   url = file.temporary_download_url
   tmpfile = "/tmp/#{submission_id}.xls"
@@ -24,15 +25,12 @@ submission_ids.each do |submission_id|
   `curl -s -o "#{tmpfile}" "#{url}"`
 
   extension = file.filename.split('.').last.downcase
-  puts "Parsing uploaded #{extension.upcase} spreadsheet"
   spreadsheet = Roo::Spreadsheet.open(tmpfile, extension: extension)
-
-  puts "Spreadsheet has sheets: #{spreadsheet.sheets.to_sentence}"
 
   invoices_sheet = spreadsheet.sheet('InvoicesRaised')
   dmsid_column = invoices_sheet.row(1).index('Digital Marketplace Service ID')
 
-  invoices_sheet.reject{ |row| row.compact.empty? }.each_with_index do |row, idx|
+  invoices_sheet.reject { |row| row.compact.empty? }.each_with_index do |row, idx|
     next if idx.zero? # Skip header row - ruby iterator zero-indexed
 
     invoice_entry = invoice_entries.find_by("CAST(source->>'row' AS INTEGER) = ?", idx + 1) # Excel rows are 1-indexed
@@ -40,7 +38,9 @@ submission_ids.each do |submission_id|
     next if invoice_entry.data['Digital Marketplace Service ID'].nil?
 
     corrected_dmsid = row[dmsid_column].to_s.delete(' ')
-    puts "Invoice Row ##{idx + 1} Entry ##{invoice_entry.id} - #{invoice_entry.data['Digital Marketplace Service ID']} => #{corrected_dmsid}"
+    puts "Invoice Row ##{idx + 1} Entry ##{invoice_entry.id} => #{corrected_dmsid}"
+
+    entries_to_correct[invoice_entry.id] = corrected_dmsid
   end
 
   orders_sheet = if spreadsheet.sheets.include?('Contracts')
@@ -52,7 +52,7 @@ submission_ids.each do |submission_id|
                  end
   dmsid_column = orders_sheet.row(1).index('Digital Marketplace Service ID')
 
-  orders_sheet.reject{ |row| row.compact.empty? }.each_with_index do |row, idx|
+  orders_sheet.reject { |row| row.compact.empty? }.each_with_index do |row, idx|
     next if idx.zero? # Skip header row - ruby iterator zero-indexed
 
     order_entry = order_entries.find_by("CAST(source->>'row' AS INTEGER) = ?", idx + 1) # Excel rows are 1-indexed
@@ -60,8 +60,18 @@ submission_ids.each do |submission_id|
     next if order_entry.data['Digital Marketplace Service ID'].nil?
 
     corrected_dmsid = row[dmsid_column].to_s.delete(' ')
-    puts "Order   Row ##{idx + 1} Entry ##{order_entry.id} - #{order_entry.data['Digital Marketplace Service ID']} => #{corrected_dmsid}"
+    puts "Order   Row ##{idx + 1} Entry ##{order_entry.id} => #{corrected_dmsid}"
+    entries_to_correct[order_entry.id] = corrected_dmsid
   end
 
   File.delete(tmpfile)
+end
+
+datestamp = Time.zone.now.strftime('%Y%m%d%H%M%S')
+
+CSV.open(Rails.root.join('db', 'data_migrate', "#{datestamp}_fix_dmsid.csv"), 'wb') do |csv|
+  csv << ['entry_id', 'dmsid']
+  entries_to_correct.each_pair do |id, value|
+    csv << [id, value]
+  end
 end
