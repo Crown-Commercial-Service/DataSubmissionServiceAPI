f52ac06c6bf0689d4303640a75b88a6a30b541d7
Russell Garner <rgarner@zephyros-systems.co.uk>
2019-04-08 17:42:31 +0100

    Add RM1557ix FDL
    Requires that we stop validating Digital Marketplace Service ID as an
    integer for now because it's going to ProductCode as a string

--- app/models/framework/definition/rm1557ix.rb
+++ app/models/framework/definition/rm1557ix.rb
@@ -81,18 +81,18 @@ class Framework
         field 'Buyer URN', :integer, exports_to: 'CustomerURN', urn: true
         field 'Buyer Organisation', :string, exports_to: 'CustomerName', presence: true
         field 'Buyer Post Code', :string, exports_to: 'CustomerPostCode', presence: true
-        field 'Invoice Date', :string, exports_to: 'CustomerInvoiceDate', ingested_date: true, presence: true
+        field 'Invoice Date', :string, exports_to: 'CustomerInvoiceDate', ingested_date: true
         field 'Invoice Number', :string, exports_to: 'InvoiceNumber', presence: true
         field 'Lot Number', :string, exports_to: 'LotNumber', presence: true, lot_in_agreement: true
         field 'Service Group', :string, exports_to: 'ProductGroup', presence: true, dependent_field_inclusion: { parent: 'Lot Number', in: MAPPING }
-        field 'Digital Marketplace Service ID', :string, exports_to: 'ProductCode', ingested_numericality: { only_integer: true }, presence: true
-        field 'UNSPSC', :string, exports_to: 'UNSPSC', ingested_numericality: { only_integer: true }, presence: true
+        field 'Digital Marketplace Service ID', :string, exports_to: 'ProductCode', presence: true
+        field 'UNSPSC', :string, exports_to: 'UNSPSC', ingested_numericality: { only_integer: true }
         field 'Unit of Purchase', :string, exports_to: 'UnitType', case_insensitive_inclusion: { in: UNIT_OF_MEASURE_VALUES }
-        field 'Price per Unit', :string, exports_to: 'UnitPrice', ingested_numericality: true, presence: true
-        field 'Quantity', :string, exports_to: 'UnitQuantity', ingested_numericality: true, presence: true
-        field 'Total Charge (Ex VAT)', :string, exports_to: 'InvoiceValue', ingested_numericality: true, presence: true
-        field 'VAT amount charged', :string, exports_to: 'VATCharged', ingested_numericality: true, presence: true
-        field 'Expenses/Disbursements', :string, exports_to: 'Expenses', ingested_numericality: true, presence: true
+        field 'Price per Unit', :string, exports_to: 'UnitPrice', ingested_numericality: true
+        field 'Quantity', :string, exports_to: 'UnitQuantity', ingested_numericality: true
+        field 'Total Charge (Ex VAT)', :string, exports_to: 'InvoiceValue', ingested_numericality: true
+        field 'VAT amount charged', :string, exports_to: 'VATCharged', ingested_numericality: true
+        field 'Expenses/Disbursements', :string, exports_to: 'Expenses', ingested_numericality: true
       end
 
       class Order < EntryData
@@ -105,12 +105,12 @@ class Framework
         field 'Buyer Contact Name', :string
         field 'Buyer Contact Number', :string
         field 'Buyer Email Address', :string
-        field 'Buyer Call Off Start Date', :string, exports_to: 'ContractStartDate', ingested_date: true, presence: true
-        field 'Call Off End Date', :string, exports_to: 'ContractEndDate', ingested_date: true, presence: true
+        field 'Buyer Call Off Start Date', :string, exports_to: 'ContractStartDate', ingested_date: true
+        field 'Call Off End Date', :string, exports_to: 'ContractEndDate', ingested_date: true
         field 'Lot Number', :string, exports_to: 'LotNumber', presence: true, lot_in_agreement: true
         field 'Service Group', :string, exports_to: 'ProductGroup', presence: true, dependent_field_inclusion: { parent: 'Lot Number', in: MAPPING }
-        field 'Digital Marketplace Service ID', :string, exports_to: 'ProductCode', ingested_numericality: { only_integer: true }, presence: true
-        field 'Call Off Value', :string, exports_to: 'ContractValue', ingested_numericality: true, presence: true
+        field 'Digital Marketplace Service ID', :string, exports_to: 'ProductCode', presence: true
+        field 'Call Off Value', :string, exports_to: 'ContractValue', ingested_numericality: true
       end
     end
   end
