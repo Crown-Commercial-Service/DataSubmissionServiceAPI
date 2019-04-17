c3aabdab6d7ce591f45d72003a41c2ea05723de0
Robert Lee-Cann <leeky@leeky.org.uk>
2019-04-17 14:06:19 +0100

    Refactor SubmissionFileDownloader#perform
    Instead of having to read values off the instance, 'perform' returns a
    Download object with the following attributes:
    
      - successful? - is true if the curl command completed successfully
      - temp_file - the filename of the temporary file containing the
        downloaded file
    
    Additionally, it exposes the 'completed?' method, which checks that the
    temp_file exists.

--- app/models/ingest/submission_file_downloader.rb
+++ app/models/ingest/submission_file_downloader.rb
@@ -3,26 +3,31 @@ require 'English'
 
 module Ingest
   ##
-  # Given a +submission_file+, fetch its attached spreadsheet using curl,
-  # exposing the local path to the downloaded attachment as +temp_file+.
+  # Given a +submission_file+, fetch its attached spreadsheet using curl.
+  #
+  # Returns a +Download+ object that exposes the local path to the
+  # downloaded attachment as +temp_file+ and a boolean +downloaded?+
+  # which is true when the file downloaded completed successfully
   class SubmissionFileDownloader
-    attr_reader :temp_file, :submission_file
+    Download = Struct.new(:temp_file, :successful?) do
+      def downloaded?
+        File.exist?(temp_file) && successful?
+      end
+    end
 
     def initialize(submission_file)
       @submission_file = submission_file
-      @url = @submission_file.file.service_url
-      @extension = @submission_file.file.filename.extension
-      @temp_file = "/tmp/#{@submission_file.id}.#{@extension}"
     end
 
     def perform
-      command = "curl -L \"#{@url}\" > \"#{@temp_file}\""
-      runner = Ingest::CommandRunner.new(command).run!
-      @successful = runner.successful?
-    end
+      url = @submission_file.file.service_url
+      extension = @submission_file.file.filename.extension
+      temp_file = "/tmp/#{@submission_file.id}.#{extension}"
 
-    def completed?
-      File.exist?(@temp_file) && @successful
+      command = "curl -L \"#{url}\" > \"#{temp_file}\""
+      runner = Ingest::CommandRunner.new(command).run!
+
+      Download.new(temp_file, runner.successful?)
     end
   end
 end
--- spec/models/ingest/submission_file_downloader_spec.rb
+++ spec/models/ingest/submission_file_downloader_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe Ingest::SubmissionFileDownloader do
 
   subject(:downloader) { Ingest::SubmissionFileDownloader.new(file) }
 
-  describe '.completed?' do
+  describe '.downloaded?' do
     let(:thread) { double('thread') }
     let(:thread_value) { double('thread_value') }
 
@@ -16,9 +16,9 @@ RSpec.describe Ingest::SubmissionFileDownloader do
       allow(Open3).to receive(:popen3).with(/curl/).and_yield(nil, [], [], thread)
       allow(File).to receive(:exist?).with('/tmp/ca52d38f-00b2-457a-888c-14a878f29897.xls').and_return(true)
 
-      downloader.perform
+      result = downloader.perform
 
-      expect(downloader.completed?).to be_truthy
+      expect(result).to be_downloaded
     end
 
     it "is false if the submission's file was not downloaded" do
@@ -27,9 +27,9 @@ RSpec.describe Ingest::SubmissionFileDownloader do
       allow(Open3).to receive(:popen3).with(/curl/).and_yield(nil, [], [], thread)
       allow(File).to receive(:exist?).with('/tmp/ca52d38f-00b2-457a-888c-14a878f29897.xls').and_return(false)
 
-      downloader.perform
+      result = downloader.perform
 
-      expect(downloader.completed?).to be_falsy
+      expect(result).to_not be_downloaded
     end
 
     it 'is false if a non-zero exit code was received' do
@@ -38,20 +38,16 @@ RSpec.describe Ingest::SubmissionFileDownloader do
       allow(Open3).to receive(:popen3).with(/curl/).and_yield(nil, [], [], thread)
       allow(File).to receive(:exist?).with('/tmp/ca52d38f-00b2-457a-888c-14a878f29897.xls').and_return(false)
 
-      downloader.perform
+      result = downloader.perform
 
-      expect(downloader.completed?).to be_falsy
-    end
-
-    it 'is false before the download has started' do
-      expect(downloader.completed?).to be_falsy
+      expect(result).to_not be_downloaded
     end
   end
 
   describe '.temp_file' do
     context 'with an XLS file' do
       it 'returns the full path to the downloaded file, with the correct extension' do
-        expect(downloader.temp_file).to eql '/tmp/ca52d38f-00b2-457a-888c-14a878f29897.xls'
+        expect(downloader.perform.temp_file).to eql '/tmp/ca52d38f-00b2-457a-888c-14a878f29897.xls'
       end
     end
 
@@ -59,14 +55,8 @@ RSpec.describe Ingest::SubmissionFileDownloader do
       let(:file) { create(:submission_file, :with_attachment, filename: 'test.xlsx', id: file_id) }
 
       it 'returns the full path to the downloaded file, with the correct extension' do
-        expect(downloader.temp_file).to eql '/tmp/ca52d38f-00b2-457a-888c-14a878f29897.xlsx'
+        expect(downloader.perform.temp_file).to eql '/tmp/ca52d38f-00b2-457a-888c-14a878f29897.xlsx'
       end
-    end
-  end
-
-  describe '.submission_file' do
-    it 'returns to SubmissionFile that was downloaded' do
-      expect(downloader.submission_file).to eql(file)
     end
   end
 end
