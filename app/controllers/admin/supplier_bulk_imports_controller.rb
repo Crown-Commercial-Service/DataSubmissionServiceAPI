fdf61cc282b5de8e3034525fc5e576d33c2d3694
Robert Lee-Cann <leeky@leeky.org.uk>
2019-05-17 11:56:37 +0100

    Allow admins to bulk import suppliers
    Using the `Import::FrameworkSuppliers` class that we've used to manually
    bulk-import suppliers from a CSV, this allows admins to perform this
    function through the UI.

---
+++ app/controllers/admin/supplier_bulk_imports_controller.rb
@@ -0,0 +1,26 @@
+class Admin::SupplierBulkImportsController < AdminController
+  def new; end
+
+  def create
+    return redirect_to new_admin_supplier_bulk_import_path, alert: 'Uploaded file is not a CSV file' unless csv?
+
+    csv_path = uploaded_file.tempfile.path
+    Import::FrameworkSuppliers.new(csv_path, logger: Rails.logger).run
+
+    redirect_to new_admin_supplier_bulk_import_path, notice: 'Successfully imported suppliers'
+  rescue ActionController::ParameterMissing
+    redirect_to new_admin_supplier_bulk_import_path, alert: 'Please choose a file to upload'
+  rescue ArgumentError => e
+    redirect_to new_admin_supplier_bulk_import_path, alert: e.message
+  end
+
+  private
+
+  def uploaded_file
+    params.require(:bulk_import).require(:csv_file)
+  end
+
+  def csv?
+    uploaded_file.content_type == 'text/csv'
+  end
+end
