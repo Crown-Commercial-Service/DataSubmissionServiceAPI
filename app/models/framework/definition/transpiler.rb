99fd4a3a629ee8f4ee47fa06bdd56ebab7ca5fe4
Russell Garner <rgarner@zephyros-systems.co.uk>
2019-06-01 10:15:12 +0100

    Remove rubocop override on management charge field
    It's needed in entry_data_class because the method needs to resemble a
    class definition, but it's not needed in
    raise_when_management_field_optional (renamed from
    check_management_charge_calculator_fields_are_not_optional to be shorter
    and to replace the verb 'check' which can be ambiguous in implying that
    a flag might be set or a return value could be supplied)

--- app/models/framework/definition/transpiler.rb
+++ app/models/framework/definition/transpiler.rb
@@ -14,7 +14,7 @@ class Framework
         ast = @ast
         transpiler = self
 
-        check_management_charge_calculator_fields_are_not_optional(ast[:management_charge])
+        raise_when_management_field_optional(ast[:management_charge])
 
         Class.new(Framework::Definition::Base) do
           framework_name       ast[:framework_name]
@@ -83,19 +83,13 @@ class Framework
         end
       end
 
-      # rubocop:disable Metrics/AbcSize
-      def check_management_charge_calculator_fields_are_not_optional(info)
-        if info[:column_based] && ast.field_by_sheet_name(:invoice, info[:column_based][:column_name]).field_def[:optional]
-          raise Transpiler::Error,
-                'Management charge references ' \
-                "'#{info[:column_based][:column_name]}' so it cannot be optional"
-        elsif info[:flat_rate] && info[:flat_rate][:column].present? && ast.field_by_sheet_name(:invoice, info[:flat_rate][:column]).field_def[:optional]
-          raise Transpiler::Error,
-                'Management charge references ' \
-                "'#{info[:flat_rate][:column]}' so it cannot be optional"
+      def raise_when_management_field_optional(info)
+        optional_found = [info.dig(:column_based, :column_name), info.dig(:flat_rate, :column)].compact.find do |field_name|
+          ast.field_by_sheet_name(:invoice, field_name)&.optional?
         end
+
+        raise Transpiler::Error, "Management charge references '#{optional_found}' so it cannot be optional" if optional_found
       end
-      # rubocop:enable Metrics/AbcSize
     end
   end
 end
