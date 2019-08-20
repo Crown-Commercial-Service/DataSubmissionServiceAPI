11f0f504d9a66dfee86598d12850aaab8b34f082
James Coglan <james.coglan@dxw.com>
2019-08-20 11:54:11 +0100

    (chore) Prevent null values in `frameworks.definition_source`
    This column has now been back-filled in production with the
    corresponding FDL files that exist in `app/models/framework`. We should
    not allow any further frameworks to be created without their FDL source
    code in the database.

--- spec/models/framework_spec.rb
+++ spec/models/framework_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe Framework do
   it { is_expected.to have_many(:submissions) }
 
   describe 'validations' do
-    subject { Framework.create(short_name: 'test') }
+    subject { create(:framework, short_name: 'test') }
     it { is_expected.to validate_presence_of(:short_name) }
     it { is_expected.to validate_uniqueness_of(:short_name) }
 
