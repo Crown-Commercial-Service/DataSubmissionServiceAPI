ee9e5a1b4664beca86d4529750c87b6100ebaeaf
Ed Davey <ed@veryreal.co.uk>
2019-12-06 12:08:20 +0000

    Apply index concurrently

--- db/migrate/20191204094848_add_index_on_submissions_created_at.rb
+++ db/migrate/20191204094848_add_index_on_submissions_created_at.rb
@@ -1,7 +1,10 @@
 class AddIndexOnSubmissionsCreatedAt < ActiveRecord::Migration[5.2]
+  disable_ddl_transaction!
+
   def change
     add_index :submissions,
               :created_at,
-              order: { created_at: :desc }
+              order: { created_at: :desc },
+              algorithm: :concurrently
   end
 end
