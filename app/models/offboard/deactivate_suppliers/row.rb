f67c516b5338c6fd744a736dadc7722fb63d51ba
George Eaton <george@dxw.com>
2020-06-22 10:41:13 +0100

    Add framework Supplier deactivation models

---
+++ app/models/offboard/deactivate_suppliers/row.rb
@@ -0,0 +1,37 @@
+module Offboard
+  class DeactivateSuppliers
+    class Row
+      attr_reader :supplier_name, :salesforce_id, :coda_reference, :framework_short_name
+
+      def initialize(supplier_name:, salesforce_id:, coda_reference:, framework_short_name:)
+        @supplier_name = supplier_name
+        @salesforce_id = salesforce_id
+        @coda_reference = coda_reference
+        @framework_short_name = framework_short_name
+      end
+
+      def offboard!
+        find_supplier.tap do |supplier|
+          break unless supplier
+
+          agreement = find_agreement(supplier)
+          agreement.deactivate!
+        end
+      end
+
+      private
+
+      def framework
+        @framework ||= Framework.published.find_by!(short_name: framework_short_name)
+      end
+
+      def find_supplier
+        Supplier.find_by(salesforce_id: salesforce_id)
+      end
+
+      def find_agreement(supplier)
+        supplier.agreements.find_by!(framework: framework)
+      end
+    end
+  end
+end
