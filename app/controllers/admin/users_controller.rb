8b57fa2fdb3f9714eb3659a443c7cc13ef33a023
Tom Berey <tomberey1@gmail.com>
2020-11-26 14:14:41 +0000

    Fix/RMI-243 Add conditional and comment

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,15 +1,12 @@
 class Admin::UsersController < AdminController
   def index
     @users = User.search(params[:search]).page(params[:page])
-    
     # The following block (Lines 8 to 11), will check for stuck submissions in 'processing' longer than 24hrs.
     # These submissions are collated into array 'submissions_stuck'. If non-empty, this array is then looped
     # through to update them as 'failed' (in a database query), to then be resubmitted by a supplier (or not).
     submissions_stuck = Submission.joins(:task).where(
-      "aasm_state = 'processing' and submissions.updated_at < ? and tasks.status != 'completed'", Time.zone.now - 1.day
-    ) 
+      "aasm_state = 'processing' and submissions.updated_at < ? and tasks.status != 'completed'", Time.zone.now - 1.day) 
     submissions_stuck.each { |s| s.update!(aasm_state: :ingest_failed) } if submissions_stuck.length.positive?
-
   end
 
   def show
