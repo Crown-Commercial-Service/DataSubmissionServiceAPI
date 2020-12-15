18eebc7e0d4421ddaf7837a46a438f78ec8feee1
Tom Berey <tomberey1@gmail.com>
2020-12-15 17:09:27 +0000

    Fix/RMI-5-FDL-Additional-Field-Fix

--- app/models/framework/definition/ast/creator.rb
+++ app/models/framework/definition/ast/creator.rb
@@ -40,7 +40,7 @@ class Framework
           { kind: :additional, type: type, field: field.to_s, from: from }
         end
         rule(type_def: subtree(:type), field: simple(:field), from: subtree(:from), depends_on: subtree(:depends_on)) do
-          { kind: :additional, type: type, field: field.to_s, from: from.to_s, depends_on: depends_on }
+          { kind: :additional, type: type, field: field.to_s, from: from, depends_on: depends_on }
         end
 
         # Unknown fields rules
