272d13acb6e3a1c0573069e0aa9ecec56be6af22
Reem Ibrahim <reem.ibrahim@co-cc02839.ad.cabinetoffice.gov.uk>
2020-12-23 10:19:25 +0000

    addressed code climate concerns

--- app/models/task/unfinished_user_notification_list.rb
+++ app/models/task/unfinished_user_notification_list.rb
@@ -3,8 +3,8 @@ require 'csv'
 class Task
   # Used to generate a 'unfinished' CSV for all users
   # of suppliers with unfinished submissions in the
-  # given year/month period, due to ingest or validation 
-  # failure, or still being in review. Outputs via +puts+ 
+  # given year/month period, due to ingest or validation
+  # failure, or still being in review. Outputs via +puts+
   # objects that respond_to? it (+STDOUT+ or +File+ being usual)
   class UnfinishedUserNotificationList
     HEADER = ['email address', 'person_name', 'supplier_name', 'task_name', 'task_status'].freeze
@@ -22,7 +22,7 @@ class Task
     delegate :info, :warn, to: :logger
 
     def generate
-    #   logger.info "Generating contacts with unfinished submissions for #{year}, #{month}"
+      logger.info "Generating contacts with unfinished submissions."
 
       output.puts(CSV.generate_line(HEADER))
 
@@ -39,28 +39,31 @@ class Task
 
     def csv_line_for(user, supplier, submission)
       CSV.generate_line(
-        [user.email, user.name, supplier.name, task_name(submission), task_status(submission)] 
+        [user.email, user.name, supplier.name, task_name(submission), task_status(submission)]
       )
     end
 
     def task_month_and_year(task)
-        "#{Date::MONTHNAMES[task.period_month]} #{task.period_year}"
+      "#{Date::MONTHNAMES[task.period_month]} #{task.period_year}"
     end
-    
+
     def task_name(submission)
-        "#{submission.task.framework.short_name} - #{submission.task.framework.name} - #{task_month_and_year(submission.task)}"
+      task = submission.task
+      "#{task.framework.short_name} - #{task.framework.name} - #{task_month_and_year(task)}"
     end
 
     def task_status(submission)
-        "#{submission.aasm_state}"
+      submission.aasm_state.to_s
     end
 
     def suppliers
-        Supplier.includes(:active_users, :submissions).joins(:submissions).merge(unfinished_submissions_relation).distinct
+      # rubocop:disable Metrics/LineLength
+      Supplier.includes(:active_users, :submissions).joins(:submissions).merge(unfinished_submissions_relation).distinct
+      # rubocop:enable Metrics/LineLength
     end
 
     def unfinished_submissions_relation
-        @unfinished_submissions_relation ||= Submission.where(aasm_state: UNFINISHED_STATUSES)
+      @unfinished_submissions_relation ||= Submission.where(aasm_state: UNFINISHED_STATUSES)
     end
   end
 end
