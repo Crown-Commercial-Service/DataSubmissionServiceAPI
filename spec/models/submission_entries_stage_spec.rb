172a71f80d4a8db0c6d4c69e52b1ed3e3456afb3
@tberey <tomberey1@gmail.com>
2022-04-21 14:19:36 +0100

    RMI-501-Create-Staging-Table-And-Update-Invoices-Export

--- spec/models/submission_entries_stage_spec.rb
+++ spec/models/submission_entries_stage_spec.rb
@@ -7,9 +7,9 @@ RSpec.describe SubmissionEntriesStage do
   it { is_expected.to validate_presence_of(:data) }
 
   describe 'sheet scope' do
-    let(:sheet_1_entry) { FactoryBot.create(:submission_entries sheet_name: 'Sheet 1') }
-    let(:another_sheet_1_entry) { FactoryBot.create(:submission_entries, sheet_name: 'Sheet 1') }
-    let(:sheet_2_entry) { FactoryBot.create(:submission_entries, sheet_name: 'Sheet 2') }
+    let(:sheet_1_entry) { FactoryBot.create(:submission_entry sheet_name: 'Sheet 1') }
+    let(:another_sheet_1_entry) { FactoryBot.create(:submission_entry, sheet_name: 'Sheet 1') }
+    let(:sheet_2_entry) { FactoryBot.create(:submission_entry, sheet_name: 'Sheet 2') }
 
     it 'returns entries for the specified sheet' do
       expect(SubmissionEntriesStage.sheet('Sheet 1')).to contain_exactly(sheet_1_entry, another_sheet_1_entry)
@@ -22,9 +22,9 @@ RSpec.describe SubmissionEntriesStage do
     let(:health_dept) { FactoryBot.create(:customer, :central_government, name: 'Department for Health') }
     let(:bobs_charity) { FactoryBot.create(:customer, :wider_public_sector, name: 'Bob’s Charity') }
 
-    let!(:home_office_entry) { FactoryBot.create(:submission_entries, customer_urn: home_office.urn) }
-    let!(:health_dept_entry) { FactoryBot.create(:submission_entries, customer_urn: health_dept.urn) }
-    let!(:bobs_charity_entry) { FactoryBot.create(:submission_entries, customer_urn: bobs_charity.urn) }
+    let!(:home_office_entry) { FactoryBot.create(:submission_entry, customer_urn: home_office.urn) }
+    let!(:health_dept_entry) { FactoryBot.create(:submission_entry, customer_urn: health_dept.urn) }
+    let!(:bobs_charity_entry) { FactoryBot.create(:submission_entry, customer_urn: bobs_charity.urn) }
 
     it 'return entries for the specified sectors' do
       expect(SubmissionEntriesStage.central_government).to contain_exactly(home_office_entry, health_dept_entry)
