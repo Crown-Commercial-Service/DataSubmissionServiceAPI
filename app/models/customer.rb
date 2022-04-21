8b783f91e5d4253630253c877ff05324b180148d
@tberey <tomberey1@gmail.com>
2022-04-21 15:31:06 +0100

    RMI-501-Create-Staging-Table-And-Update-Invoices-Export

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -2,6 +2,7 @@ class Customer < ApplicationRecord
   enum sector: { central_government: 0, wider_public_sector: 1 }
 
   has_many :submission_entries,
+           :submission_entries_stages,
            primary_key: :urn,
            foreign_key: :customer_urn,
            inverse_of: :customer,
---
+++ db/data_migrate/20181109141114_fix_submission_entries_total_values.rb
@@ -0,0 +1,27 @@
+require 'progress_bar'
+
+Framework.all.find_each do |framework|
+  puts framework.short_name
+
+  total_value_field = framework.definition::Invoice.total_value_field
+
+  entries = SubmissionEntry
+            .validated
+            .invoices
+            .where(
+              "submission_entries_stages.total_value <> \
+               TRANSLATE(submission_entries_stages.data->>'#{total_value_field}', '$£ ,', '')::decimal"
+            )
+            .joins(submission: :framework)
+            .merge(Framework.where(short_name: framework.short_name))
+
+  bar = ProgressBar.new(entries.count)
+
+  entries.find_each do |entry|
+    total_value = BigDecimal(entry.data[total_value_field].tr('$£, ', ''))
+    management_charge = framework.definition.management_charge(total_value)
+
+    entry.update!(total_value: total_value, management_charge: management_charge)
+    bar.increment!
+  end
+end
