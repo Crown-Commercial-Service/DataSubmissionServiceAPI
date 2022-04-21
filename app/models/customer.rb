ec6ece7eca6f8277fea20d1c21d70002057187be
@tberey <tomberey1@gmail.com>
2022-04-21 15:46:10 +0100

    RMI-501-Create-Staging-Table-And-Update-Invoices-Export

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -2,7 +2,12 @@ class Customer < ApplicationRecord
   enum sector: { central_government: 0, wider_public_sector: 1 }
 
   has_many :submission_entries,
-           :submission_entries_stages,
+           primary_key: :urn,
+           foreign_key: :customer_urn,
+           inverse_of: :customer,
+           dependent: :restrict_with_error
+
+  has_many :submission_entries_stages,
            primary_key: :urn,
            foreign_key: :customer_urn,
            inverse_of: :customer,
