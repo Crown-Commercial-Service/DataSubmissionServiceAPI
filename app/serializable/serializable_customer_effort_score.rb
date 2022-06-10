97fa2da934ea21d06a3319b184b02b8050546a0f
Reem Ibrahim <reem.ibrahim@crowncommercial.gov.uk>
2022-06-10 12:08:11 +0100

    removed unnecessary serializable class

--- app/serializable/serializable_customer_effort_score.rb
+++
@@ -1,7 +0,0 @@
-# class SerializableCustomerEffortScore < JSONAPI::Serializable::Resource
-#   type 'customer_effort_scores'
-
-#   belongs_to :user
-
-#   attributes :ratings, :comments, :user_id
-# end
