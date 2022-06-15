ce586f117fd00595062bd714b83b69279675075b
Reem Ibrahim <reem.ibrahim@crowncommercial.gov.uk>
2022-06-15 15:07:53 +0100

    moved rating to integer method to the backend

--- app/controllers/v1/customer_effort_scores_controller.rb
+++ app/controllers/v1/customer_effort_scores_controller.rb
@@ -4,12 +4,24 @@ class V1::CustomerEffortScoresController < APIController
   def create
     feedback = params.dig(:customer_effort_score)
     score = CustomerEffortScore.new(
-      rating: feedback[:rating], 
+      rating: rating_to_integer(feedback[:rating]), 
       comments: feedback[:comments], 
       user_id: feedback[:user_id]
     )
     score.save!
 
     head :no_content
+  end
+
+  private
+
+  def rating_to_integer(feedback)
+    case feedback
+    when 'Very easy' then 5
+    when 'Easy' then 4
+    when 'Neither easy or hard' then 3
+    when 'Hard' then 2
+    when 'Very hard' then 1
+    end
   end
 end
