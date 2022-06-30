3bbeeab5fc55c64df01063b3d220528473ba2817
Reem Ibrahim <reem.ibrahim@crowncommercial.gov.uk>
2022-06-30 11:52:04 +0100

    addressed code cilmate issue

--- app/models/customer_effort_score.rb
+++ app/models/customer_effort_score.rb
@@ -2,6 +2,5 @@ class CustomerEffortScore < ApplicationRecord
   belongs_to :user
 
   validates :rating, presence: true
-  validates :comments, length: { maximum: 2000,
-    too_long: 'Feedback must be %{count} characters or less' }
+  validates :comments, length: { maximum: 2000 }
 end
