6c419fb7e8d4858bbe02c5cad99dfbe56a1692c0
Reem Ibrahim <reem.ibrahim@crowncommercial.gov.uk>
2022-07-27 12:30:28 +0100

    added customer effort scores download and updated specs

--- app/controllers/admin/downloads_controller.rb
+++ app/controllers/admin/downloads_controller.rb
@@ -1,10 +1,21 @@
-class Admin::NotifyDownloadsController < AdminController
+class Admin::DownloadsController < AdminController
   before_action :catch_unrecognised_download, only: :show
 
   def index; end
 
   def show
     send_file csv_file, type: 'text/csv', filename: csv_filename
+  end
+
+  def new
+    return redirect_to admin_downloads_path, alert: 'Please provide valid dates' unless valid_dates?
+
+    unless valid_date_order?
+      return redirect_to admin_downloads_path,
+                         alert: "'From' date must be before 'To' date and 'To' date cannot be in the future"
+    end
+
+    send_file csv_file, type: 'text/csv', filename: "customer_effort_scores-#{@from_date}-#{@to_date}.csv"
   end
 
   private
@@ -28,6 +39,8 @@ class Admin::NotifyDownloadsController < AdminController
       Task::AnticipatedUserNotificationList.new(month: current_date.month, year: current_date.year, output: file)
     when 'unfinished'
       Task::UnfinishedUserNotificationList.new(output: file)
+    else
+      Task::CustomerEffortScoreList.new(date_from: @from_date, date_to: @to_date, output: file)
     end
   end
 
@@ -37,6 +50,17 @@ class Admin::NotifyDownloadsController < AdminController
 
   def current_date
     Time.zone.today
+  end
+
+  def valid_dates?
+    @from_date = "#{params[:dd_from]}-#{params[:mm_from]}-#{params[:yyyy_from]}".to_date
+    @to_date = "#{params[:dd_to]}-#{params[:mm_to]}-#{params[:yyyy_to]}".to_date
+  rescue ArgumentError
+    false
+  end
+
+  def valid_date_order?
+    @to_date > @from_date && @to_date <= Time.zone.now.to_date
   end
 
   def catch_unrecognised_download
