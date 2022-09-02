5ff9c5ffab8df2d9957bf3f9e01cbd34b3ebaab4
Reem Ibrahim <reem.ibrahim@crowncommercial.gov.uk>
2022-09-02 18:25:44 +0100

    addressed codeclimate issue

--- app/jobs/notify_tasks_overdue_job.rb
+++ app/jobs/notify_tasks_overdue_job.rb
@@ -2,10 +2,14 @@ require 'bank_holidays'
 
 class NotifyTasksOverdueJob < ApplicationJob
   def perform
-    return unless Time.zone.today == sixth_working_day
-
-    Task::OverdueUserNotificationList.new(month: submission_period_month, year: submission_period_year,
-                                          template_id: 'e7afcd6c-51c1-4cb0-adf5-a0947909a7a2', logger: logger).notify
+    case Time.zone.today
+    when sixth_working_day
+      Task::OverdueUserNotificationList.new(month: submission_period_month, year: submission_period_year,
+                                            template_id: 'e7afcd6c-51c1-4cb0-adf5-a0947909a7a2', logger: logger).notify
+    when eleventh_working_day
+      Task::OverdueUserNotificationList.new(month: submission_period_month, year: submission_period_year,
+                                            template_id: 'b45a8f1c-a612-4266-910f-f5ce42df2b56', logger: logger).notify
+    end
   end
 
   private
@@ -22,6 +26,12 @@ class NotifyTasksOverdueJob < ApplicationJob
     dates = Range.new(Time.zone.today.beginning_of_month, Time.zone.today.end_of_month).to_a
     dates = dates.reject { |date| date.saturday? || date.sunday? || bank_holidays.include?(date) }
     dates[5]
+  end
+
+  def eleventh_working_day
+    dates = Range.new(Time.zone.today.beginning_of_month, Time.zone.today.end_of_month).to_a
+    dates = dates.reject { |date| date.saturday? || date.sunday? || bank_holidays.include?(date) }
+    dates[10]
   end
 
   def bank_holidays
