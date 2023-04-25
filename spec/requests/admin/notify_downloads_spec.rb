c0b34225a8e354f53b47b59700280b788b997d24
Reem Ibrahim <reem.ibrahim@crowncommercial.gov.uk>
2023-04-25 11:47:42 +0100

    addressed rubocop issues

--- spec/requests/admin/notify_downloads_spec.rb
+++ spec/requests/admin/notify_downloads_spec.rb
@@ -104,7 +104,8 @@ RSpec.describe 'Admin Downloads', type: :request do
 
         expect(response).to be_successful
         expect(response.header['Content-Type']).to include 'text/csv'
-        expect(response.header['Content-Disposition']).to include 'attachment; filename="late_notifications-2019-03-09.csv"'
+        expect(response.header['Content-Disposition']).to include 'attachment; ' \
+        'filename="late_notifications-2019-03-09.csv"'
         expect(response.body).to include 'email address,due_date,person_name'
         expect(response.body).to include 'User A'
       end
@@ -134,7 +135,8 @@ RSpec.describe 'Admin Downloads', type: :request do
 
       expect(response).to be_successful
       expect(response.header['Content-Type']).to include 'text/csv'
-      expect(response.header['Content-Disposition']).to include 'attachment; filename="due_notifications-2018-02-27.csv"'
+      expect(response.header['Content-Disposition']).to include 'attachment; ' \
+      'filename="due_notifications-2018-02-27.csv"'
       expect(response.body).to include 'email address,due_date,person_name'
       expect(response.body).to include 'User A'
       expect(response.body).to include ',February 2018,'
