ce20150c42770983c7fd817f5ee60ef31874903e
Reem Ibrahim <reem.ibrahim@crowncommercial.gov.uk>
2023-06-29 12:32:43 +0100

    fixed specs

--- spec/models/workday/customer_invoice_spec.rb
+++ spec/models/workday/customer_invoice_spec.rb
@@ -9,7 +9,7 @@ RSpec.describe Workday::CustomerInvoice do
     allow(Workday).to receive(:tenant).and_return('tenant')
     allow(Workday).to receive(:username).and_return('user')
     allow(Workday).to receive(:api_password).and_return('password')
-    stub_request(:get, 'https://wd3-impl-services1.workday.com/ccx/service/customreport2/tenant/INT003_ISU/CR_Customer_Invoice_API?Customer_Invoice!WID=WORKDAY_REFERENCE#1')
+    stub_request(:get, "https://wd3-impl-services1.workday.com/ccx/service/customreport2/tenant/INT003_ISU/CR_Customer_Invoice_API?Customer_Invoice!WID=#{submission_invoice.workday_reference}")
       .with(headers: { 'Authorization' => 'Basic dXNlcjpwYXNzd29yZA==' })
       .to_return(status: 200, body: File.read(Rails.root.join('spec', 'fixtures', 'customer_invoice.xml')))
   end
@@ -25,7 +25,7 @@ RSpec.describe Workday::CustomerInvoice do
 
     context 'when the workday API returns a 500 status code' do
       it 'raises an error' do
-        stub_request(:get, 'https://wd3-impl-services1.workday.com/ccx/service/customreport2/tenant/INT003_ISU/CR_Customer_Invoice_API?Customer_Invoice!WID=WORKDAY_REFERENCE#2')
+        stub_request(:get, "https://wd3-impl-services1.workday.com/ccx/service/customreport2/tenant/INT003_ISU/CR_Customer_Invoice_API?Customer_Invoice!WID=#{submission_invoice.workday_reference}")
           .with(headers: { 'Authorization' => 'Basic dXNlcjpwYXNzd29yZA==' })
           .to_return(status: 500, body: '')
 
