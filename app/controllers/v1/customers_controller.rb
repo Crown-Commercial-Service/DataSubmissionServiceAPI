0f1ee6892861e5b73822812fc604baae42761ab9
Reem Ibrahim <reem.ibrahim@crowncommercial.gov.uk>
2023-07-18 15:33:47 +0100

    addressed rubocop

--- app/controllers/v1/customers_controller.rb
+++ app/controllers/v1/customers_controller.rb
@@ -3,7 +3,7 @@ class V1::CustomersController < ApiController
     customer_urns = Customer.where(deleted: false)
                             .where(published: true)
                             .order(:name)
-    
+
     customer_urns = customer_urns.search(params.dig(:filter, :search)) if params.dig(:filter, :search)
 
     render jsonapi: customer_urns
