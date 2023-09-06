a6a7bb63e1ade0095ecadcbce4f9bdc7063b1d7e
georgeschena <georgeschena@hotmail.com>
2023-09-06 10:40:33 +0100

    Setup to run tests for Jenkins

---
+++ docker-tests.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+COMMIT_HASH=$(git rev-parse --short HEAD)
+export COMMIT_HASH
+
+docker build -t datasubmissionserviceapi:$COMMIT_HASH .
+
+docker compose --file docker-compose.test.yml --env-file docker-compose.env build
+docker compose --file docker-compose.test.yml --env-file docker-compose.env up -d db-test
+docker compose --file docker-compose.test.yml --env-file docker-compose.env up -d test
+
+docker exec -it data-submission-service-api_test bundle exec rake default
+
+if [ $? -ne 0 ]; then
+  echo "Tests failed! Exiting with status 1..."
+  exit 1
+fi
