e8cd6d36be6c1bb4cef366c767aa239e94f05df9
georgeschena <georgeschena@hotmail.com>
2023-09-07 13:39:14 +0100

    Use docker-compose.build.yml

--- docker-tests.sh
+++ docker-tests.sh
@@ -3,11 +3,11 @@
 COMMIT_HASH=$(git rev-parse --short HEAD)
 export COMMIT_HASH
 
-docker build -t datasubmissionserviceapi:$COMMIT_HASH .
+docker build -t datasubmissionserviceapi:$COMMIT_HASH --build-arg BUNDLE_GEMS__CONTRIBSYS__COM=$1 .
 
-docker compose --file docker-compose.test.yml --env-file docker-compose.env build
-docker compose --file docker-compose.test.yml --env-file docker-compose.env up -d db-test
-docker compose --file docker-compose.test.yml --env-file docker-compose.env up -d test
+docker compose --file docker-compose.build.yml --env-file docker-compose.env build
+docker compose --file docker-compose.build.yml --env-file docker-compose.env up -d db-test
+docker compose --file docker-compose.build.yml --env-file docker-compose.env up -d test
 
 docker exec -it data-submission-service-api_test bundle exec rake default
 
@@ -16,4 +16,4 @@ if [ $? -ne 0 ]; then
   exit 1
 fi
 
-docker compose --file docker-compose.test.yml --env-file docker-compose.env down
+docker compose --file docker-compose.build.yml --env-file docker-compose.env down
