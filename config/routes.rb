d39a8f384f7f588d83f6f9aedaacbaffc69d3496
Reem Ibrahim <reem.ibrahim@crowncommercial.gov.uk>
2024-06-04 15:01:46 +0100

    created customers endpoint and spec

--- config/routes.rb
+++ config/routes.rb
@@ -63,6 +63,7 @@ Rails.application.routes.draw do
     resources :suppliers, only: :index
     resources :memberships, only: :index
     resources :agreements, only: :index
+    resources :customers, only: :index
   end
 
   namespace :admin do
