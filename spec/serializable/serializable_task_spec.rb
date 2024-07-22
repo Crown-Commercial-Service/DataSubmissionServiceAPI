a7844cea602adde418d1eb73770a389130724f34
Reem Ibrahim <reem.ibrahim@crowncommercial.gov.uk>
2024-07-22 14:59:28 +0100

    amended task serialization and added spec

---
+++ spec/serializable/serializable_task_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+RSpec.describe SerializableTask do
+  context 'given a task with past submissions' do
+    let(:user) { FactoryBot.create(:user) }
+    let(:task) { FactoryBot.create(:task, status: :completed) }
+    let(:old_submission) do
+      FactoryBot.create(
+          :submission_with_validated_entries,
+          submitted_by: user,
+          aasm_state: 'replaced',
+          task: task
+      )
+    end
+
+    it 'exposes past_submissions' do
+      task.submissions << old_submission
+      serialized_task = SerializableTask.new(object: task, include_past_submissions: true)
+
+      expect(serialized_task.as_jsonapi[:attributes][:past_submissions][0][:id]).to include(old_submission.id)
+    end
+  end
+end
