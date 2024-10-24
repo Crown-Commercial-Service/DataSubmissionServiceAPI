b6da8a468d596a35d78d09a65450535d32c996f9
Reem Ibrahim <reem.ibrahim@crowncommercial.gov.uk>
2024-10-24 10:39:29 +0100

    added past notifications log

--- spec/features/admin_can_manage_notifications_spec.rb
+++ spec/features/admin_can_manage_notifications_spec.rb
@@ -45,4 +45,24 @@ RSpec.feature 'Managing notifications' do
     expect(page).to have_text('Notification was successfully unpublished.')
     expect(page).to have_text('No notification currently published')
   end
+
+  scenario 'view past notifications' do
+    number = 0
+    2.times do
+      visit new_admin_notification_path
+      fill_in 'notification_summary', with: "Testy McTestface #{number += 1}"
+      fill_in 'notification_notification_message', with: "I'm number #{number}"
+      click_button 'Publish Notification'
+    end
+    
+    visit admin_notifications_path
+
+    expect(page).to have_text('Testy McTestface 1')
+    expect(page).to have_text('Testy McTestface 2')
+
+    click_link 'Testy McTestface 1'
+
+    expect(page).to have_text('Testy McTestface 1')
+    expect(page).to have_text('I\'m number 1')
+  end
 end
