7e5655729ac766645e11eb11ec1532ce47801fe1
Reem Ibrahim <reem.ibrahim@crowncommercial.gov.uk>
2024-11-14 14:38:29 +0000

    added edit notification feature and test

--- app/controllers/admin/notifications_controller.rb
+++ app/controllers/admin/notifications_controller.rb
@@ -2,24 +2,25 @@ require 'custom_markdown_renderer'
 
 class Admin::NotificationsController < AdminController
   def index
+    markdown_parser = Redcarpet::Markdown.new(CustomMarkdownRenderer)
     @published_notification = Notification.published.first
+    @published_notification_message = markdown_parser.render(@published_notification[:notification_message]) if @published_notification
     @notifications = Notification.order(published_at: :desc).all
   end
 
   def new
     @notification = Notification.new
+    @published_notification = Notification.find(params[:published_notification]) if params[:published_notification]
   end
 
   def show
+    markdown_parser = Redcarpet::Markdown.new(CustomMarkdownRenderer)
     @notification = Notification.find(params[:id])
+    @notification_message = markdown_parser.render(@notification[:notification_message])
   end
 
-  # rubocop:disable Metrics/AbcSize
   def create
-    renderer = CustomMarkdownRenderer.new
-    markdown_parser = Redcarpet::Markdown.new(renderer)
-    html = markdown_parser.render(notification_params[:notification_message])
-    @notification = Notification.new(summary: notification_params[:summary], notification_message: html,
+    @notification = Notification.new(summary: notification_params[:summary], notification_message: notification_params[:notification_message],
                                      user: current_user['email'], published: true, published_at: Time.zone.now)
     Notification.transaction do
       if @notification.save
@@ -31,11 +32,9 @@ class Admin::NotificationsController < AdminController
       end
     end
   end
-  # rubocop:enable Metrics/AbcSize
 
   def preview
-    renderer = CustomMarkdownRenderer.new
-    markdown_parser = Redcarpet::Markdown.new(renderer)
+    markdown_parser = Redcarpet::Markdown.new(CustomMarkdownRenderer)
     @markdown_message = markdown_parser.render(params[:message])
 
     render json: { summary: params[:summary], message: @markdown_message }
