df4849bc2d41c8961f5521f6ddba383e3849a3ea
Reem Ibrahim <reem.ibrahim@co-cc02839.ad.cabinetoffice.gov.uk>
2025-10-14 13:45:39 +0100

    replaced rake task with nightly job

---
+++ app/jobs/cleanup_submission_entries_job.rb
@@ -0,0 +1,41 @@
+class CleanupSubmissionEntriesJob < ApplicationJob
+  TASK_BATCH_SIZE = 50
+  ENTRIES_BATCH_SIZE = 1000
+
+  def perform(dry_run: false)
+    total_deleted_entries = 0
+
+    Task.find_in_batches(batch_size: TASK_BATCH_SIZE) do |tasks_batch|
+      tasks_batch.each do |task|
+        active_id = task.active_submission&.id
+        failed_submissions = task.submissions
+                                 .where(aasm_state: 'validation_failed', cleanup_processed: false)
+                                 .where.not(id: active_id)
+
+        next if failed_submissions.empty?
+
+        deleted_for_task = 0
+
+        failed_submissions.find_each do |submission|
+          submission.entries.in_batches(of: ENTRIES_BATCH_SIZE) do |entries_batch|
+            if dry_run
+              Rollbar.info("Dry run: would delete #{entries_batch.size} entries for Submission ID #{submission.id}.")
+            else
+              deleted_count = entries_batch.delete_all
+              deleted_for_task += deleted_count
+              total_deleted_entries += deleted_count
+            end
+          end
+
+          submission.update(cleanup_processed: true) unless dry_run
+        end
+
+        unless dry_run
+          Rollbar.info("Task ID #{task.id}: Processed #{failed_submissions.count} failed submissions, deleted #{deleted_for_task} entries.")
+        end
+      end
+
+      sleep 1 # To avoid overwhelming the database
+    end
+  end
+end
