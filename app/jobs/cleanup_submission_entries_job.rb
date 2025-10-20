38b8e20c7d0e309165c18ec0682f535062786716
Reem Ibrahim <reem.ibrahim@co-cc02839.ad.cabinetoffice.gov.uk>
2025-10-20 14:49:28 +0100

    updated job and added indexes

--- app/jobs/cleanup_submission_entries_job.rb
+++ app/jobs/cleanup_submission_entries_job.rb
@@ -1,11 +1,15 @@
 class CleanupSubmissionEntriesJob < ApplicationJob
-  TASK_BATCH_SIZE = 25
   ENTRIES_BATCH_SIZE = 1000
 
-  def perform(dry_run: false)
+  def perform(dry_run: false, max_run_time: 5.hours, task_batch_sixe: 100)
+    start_time = Time.current
     total_deleted_entries = 0
 
-    Task.find_in_batches(batch_size: TASK_BATCH_SIZE) do |tasks_batch|
+    tasks_with_unprocessed_submissions = Task.joins(:submissions)
+                                             .where(submissions: { aasm_state: 'validation_failed', cleanup_processed: false })
+                                             .distinct
+
+    tasks_with_unprocessed_submissions.find_in_batches(batch_size: task_batch_sixe) do |tasks_batch|
       tasks_batch.each do |task|
         active_id = task.active_submission&.id
 
@@ -18,23 +22,30 @@ class CleanupSubmissionEntriesJob < ApplicationJob
         deleted_for_task = 0
 
         failed_submissions.find_each do |submission|
-          submission.entries.in_batches(of: ENTRIES_BATCH_SIZE) do |entries_batch|
-            if dry_run
-              Rollbar.info("Dry run: would delete #{entries_batch.count} entries for Submission ID #{submission.id}.")
-            else
-              deleted_count = entries_batch.delete_all
-              deleted_for_task += deleted_count
-              total_deleted_entries += deleted_count
-              # rubocop:disable Layout/LineLength
-              Rollbar.info("Task ID #{task.id}: Processed #{failed_submissions.count} failed submissions, deleted #{deleted_for_task} entries.")
-              # rubocop:enable Layout/LineLength
+          begin
+            submission.entries.in_batches(of: ENTRIES_BATCH_SIZE) do |entries_batch|
+              if dry_run
+                Rollbar.info("Dry run: would delete #{entries_batch.count} entries for Submission ID #{submission.id}.")
+              else
+                deleted_count = entries_batch.delete_all
+                deleted_for_task += deleted_count
+                total_deleted_entries += deleted_count
+                # rubocop:disable Layout/LineLength
+                Rollbar.info("Task ID #{task.id}: Processed #{failed_submissions.count} failed submissions, deleted #{deleted_for_task} entries.")
+                # rubocop:enable Layout/LineLength
+              end
             end
+
+            submission.update(cleanup_processed: true) unless dry_run
+          rescue => e
+            Rollbar.error(e, "Error processing Submission ID #{submission.id} for Task ID #{task.id}: #{e.message}")
           end
 
-          submission.update(cleanup_processed: true) unless dry_run
+          break if Time.current - start_time >= max_run_time
         end
       end
 
+      break if Time.current - start_time >= max_run_time
       sleep 1 # To avoid overwhelming the database
     end
   end
